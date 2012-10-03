061ec73d368f032531f7580b0c18445c05abd5e4
Ben Davies <ben.davies@anu.edu.au>
2012-10-03 17:42:37 +1000

    Added main and unittest.

---
+++ src/pyrate/main_tests.py
@@ -0,0 +1,27 @@
+'''
+Created on 17/09/2012
+@author: bpd900
+'''
+
+import os, unittest
+
+import main
+
+
+
+class PyRateTests(unittest.TestCase):
+
+	def test_pyrate_main(self):
+		cwd = os.getcwd()
+		td = "../../tests/sydney_test"
+		
+		os.chdir(td)
+		
+		main.main(verbose=True)
+		
+		
+		os.chdir
+
+
+if __name__ == "__main__":
+	unittest.main()
