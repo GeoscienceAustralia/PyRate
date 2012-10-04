1b046efbb86fe128be5f140b68c4e09b17a4f0d7
Ben Davies <ben.davies@anu.edu.au>
2012-10-04 10:34:42 +1000

    Added missing __main__ to shared_tests.py which prevented tests from running.

--- src/pyrate/shared_tests.py
+++ src/pyrate/shared_tests.py
@@ -1,6 +1,5 @@
 '''
 Created on 12/09/2012
-
 @author: bpd900
 '''
 
@@ -11,5 +10,9 @@ import shared
 
 class SharedTests(unittest.TestCase):
 
-	def test_ifg(self):
+	def test_create_ifg(self):
 		ifg = shared.Ifg('../../tests/sydney_test/obs/geo_060619-061002.unw')
+
+
+if __name__ == "__main__":
+	unittest.main()
