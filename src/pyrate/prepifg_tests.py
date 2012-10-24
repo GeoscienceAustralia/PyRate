c05dfc04fb34eec8e3f04251fe35886afd74866f
Ben Davies <ben.davies@anu.edu.au>
2012-10-24 12:43:43 +1100

    Expanded test coverage for geotransform values.

--- src/pyrate/prepifg_tests.py
+++ src/pyrate/prepifg_tests.py
@@ -12,6 +12,7 @@ except:
 	import gdal
 
 import prepifg
+from shared import Ifg
 from config import OBS_DIR, IFG_CROP_OPT, IFG_LKSX, IFG_LKSY, IFG_FILE_LIST
 
 
@@ -21,6 +22,9 @@ class OutputTests(unittest.TestCase):
 	
 	def setUp(self):
 		self.testdir = "../../tests/prepifg"
+		tmp = ["obs/geo_060619-061002.unw.rsc", "obs/geo_070326-070917.unw.rsc"]
+		self.hdr_files = [join(self.testdir, t) for t in tmp]
+		
 		tmp = ["obs/geo_060619-061002_TODO.tif", "obs/geo_070326-070917_TODO.tif"]	
 		self.exp_files = [join(self.testdir, t) for t in tmp]
 		
@@ -39,11 +43,16 @@ class OutputTests(unittest.TestCase):
 		params[OBS_DIR] = join(self.testdir,"obs/")
 		
 		prepifg.prepare_ifgs(params)
-		
 		for f in self.exp_files:
 			self.assertTrue(exists(f), msg="Output files not created")
 
-		# should have 2 output files of the same bounds
+		# output files should have same extents
+		ifg = Ifg(self.exp_files[0], self.hdr_files[0])
+		ifg.open()
+		gt = ifg.dataset.GetGeoTransform()
+		exp_gt = (150.91, 0.000833333, 0, -34.17, 0, -0.000833333) # copied from gdalinfo output
+		for i,j in zip(gt, exp_gt):
+			self.assertAlmostEqual(i, j)		
 		assert_geotransform_equal(self.exp_files)
 
 
@@ -56,7 +65,8 @@ class OutputTests(unittest.TestCase):
 def assert_geotransform_equal(files):
 	"""Asserts geotransforms for the given files are equivalent. Files can be paths
 	to datasets, or GDAL dataset objects."""
-
+	
+	assert len(files) > 1, "Need more than 1 file to compare"
 	if not all( [hasattr(f, "GetGeoTransform") for f in files] ):
 		datasets = [gdal.Open(f) for f in files]
 		assert all(datasets)
