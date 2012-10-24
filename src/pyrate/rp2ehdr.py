87128ae1b8256dd7d6b3a2e34cd62c5ee7c6d30a
Ben Davies <ben.davies@anu.edu.au>
2012-10-24 13:38:56 +1100

    Fixed error handling for rp2ehdr.py script.

--- src/pyrate/rp2ehdr.py
+++ src/pyrate/rp2ehdr.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # Quick and dirty script to generate EHdr format header files from ROIPAC data
+# Ben Davies, ANUSF/NCI  24/10/12
 
-import os, sys
+import sys
 from pyrate.roipac import to_ehdr_header
 
-usage = "Usage: rp2ehdr.py [geo****.unw.rsc] file" 
+
+usage = "Usage: rp2ehdr.py [ROIPAC unw.rsc path]\n" 
 if len(sys.argv) != 2:
 	sys.stderr.write(usage)
+	sys.exit()
 
 to_ehdr_header(sys.argv[1])
