db367ab8f1847f41974802a6579e82ad717d1cf0
Ben Davies <ben.davies@anu.edu.au>
2012-10-25 15:02:13 +1100

    Added extents tests for prepifg.py script.

--- src/pyrate/prepifg.py
+++ src/pyrate/prepifg.py
@@ -3,24 +3,25 @@ Created on 23/10/2012
 @author: Ben Davies
 """
 
-import os
+import os, sys
 from os.path import join, splitext
+from math import modf
 from subprocess import check_call
 
 from shared import Ifg
 from config import OBS_DIR, IFG_CROP_OPT, IFG_LKSX, IFG_LKSY, IFG_FILE_LIST
 from config import IFG_XFIRST, IFG_XLAST, IFG_YFIRST, IFG_YLAST
-from ifgconstants import X_FIRST, Y_FIRST
+from ifgconstants import X_FIRST, Y_FIRST, X_LAST, Y_LAST
 
 # Constants
 MINIMUM_CROP = 1
 MAXIMUM_CROP = 2
 CUSTOM_CROP = 3
 NO_CROP = 4
+GRID_TOL = 1e-6
 
 
-
-def prepare_ifgs(params):
+def prepare_ifgs(params, use_exceptions=False, verbose=False):
 	with open(params[IFG_FILE_LIST]) as f:
 		filelist = f.readlines()
 	
@@ -44,11 +45,32 @@ def prepare_ifgs(params):
 		ymin = min([i.Y_LAST for i in ifgs])
 
 	elif crop_opt == CUSTOM_CROP:
-		# TODO: warn if the extents don't match up with the grid
 		xmin = params[IFG_XFIRST]
 		ymax = params[IFG_YFIRST]
 		xmax = params[IFG_XLAST]
 		ymin = params[IFG_YLAST]
+		
+		# check cropping coords line up with grid system within tolerance
+		# NB: only tests against the first Ifg
+		i = ifgs[0]
+		for par, crop, step in zip([X_FIRST, X_LAST, Y_FIRST, Y_LAST],
+															[xmin, xmax, ymax, ymin],
+															[i.X_STEP, i.X_STEP, i.Y_STEP, i.Y_STEP]):			
+			
+			# is diff of the given extent from grid a multiple of X|Y_STEP ?
+			param = getattr(i, par)
+			diff = abs(crop - param)
+			remainder = abs(modf(diff / step)[0])
+			
+			# handle cases where division gives remainder near zero, or just < 1
+			if remainder > GRID_TOL and remainder < (1 - GRID_TOL):								
+				msg = "%s crop extent is not within %s of the grid coords" % (par, GRID_TOL)
+				if use_exceptions:
+					raise PreprocessingException(msg)
+				else:
+					sys.stderr.write("WARN: ")
+					sys.stderr.write(msg)
+					sys.stderr.write('\n')
 		
 	elif crop_opt == NO_CROP:
 		raise NotImplementedError("Same as maximum for IFGs of same size")
@@ -64,6 +86,7 @@ def prepare_ifgs(params):
 		s = splitext(i.data_path)
 		looks_path = s[0] + "_TODO" + ".tif" 
 		args = ["gdalwarp", "-te"] + extents + [i.data_path, looks_path]
+		if not verbose: args.append("-q")
 		check_call(args)	
 
 
--- src/pyrate/prepifg_tests.py
+++ src/pyrate/prepifg_tests.py
@@ -11,6 +11,8 @@ try:
 except:
 	import gdal
 
+gdal.UseExceptions()
+
 import prepifg
 from shared import Ifg
 from config import OBS_DIR, IFG_CROP_OPT, IFG_LKSX, IFG_LKSY, IFG_FILE_LIST
@@ -21,17 +23,35 @@ class OutputTests(unittest.TestCase):
 	"""TODO"""
 	
 	def setUp(self):
+		self.xs = 0.000833333
+		self.ys = -self.xs
+		
 		self.testdir = "../../tests/prepifg"
 		tmp = ["obs/geo_060619-061002.unw.rsc", "obs/geo_070326-070917.unw.rsc"]
 		self.hdr_files = [join(self.testdir, t) for t in tmp]
 		
 		tmp = ["obs/geo_060619-061002_TODO.tif", "obs/geo_070326-070917_TODO.tif"]	
 		self.exp_files = [join(self.testdir, t) for t in tmp]
-		
-		# clear tmp test files before each run
+
+
+	def tearDown(self):
+		# clear tmp output files after each run
 		for f in self.exp_files:
 			if exists(f):
 				os.remove(f)
+
+
+	def _custom_extents_param(self):
+		"""Convenience function to create custom cropping extents params"""
+		
+		params = {IFG_CROP_OPT: prepifg.CUSTOM_CROP, IFG_LKSX: 1, IFG_LKSY: 1}		
+		params[IFG_XFIRST] = 150.91 + (7 * self.xs)
+		params[IFG_YFIRST] = -34.17 + (16 * self.ys)
+		params[IFG_XLAST] = 150.91 + (27 * self.xs) # 20 cells across from X_FIRST
+		params[IFG_YLAST] = -34.17 + (44 * self.ys) # 28 cells across from Y_FIRST
+		params[IFG_FILE_LIST] = join(self.testdir, 'obs/ifms')
+		params[OBS_DIR] = join(self.testdir,"obs/")
+		return params
 
 
 	def test_default_max_extents(self):
@@ -64,38 +84,41 @@ class OutputTests(unittest.TestCase):
 		params[IFG_FILE_LIST] = join(self.testdir, 'obs/ifms')
 		params[OBS_DIR] = join(self.testdir,"obs/")
 		
-		# output files should have same extents
 		prepifg.prepare_ifgs(params)
 		ifg = Ifg(self.exp_files[0], self.hdr_files[0])
 		ifg.open()
+		
+		# output files should have same extents
 		gt = ifg.dataset.GetGeoTransform()
-		exp_gt = (150.911666666, 0.000833333, 0, -34.174166665, 0, -0.000833333) # copied from gdalinfo output
+		exp_gt = (150.911666666, 0.000833333, 0, -34.172499999, 0, -0.000833333) # copied from gdalinfo output
 		for i,j in zip(gt, exp_gt):
 			self.assertAlmostEqual(i, j)
 		assert_geotransform_equal(self.exp_files)
 	
 	
 	def test_custom_extents(self):
-		# create dummy params file (relative paths to prevent chdir calls)
-		params = {IFG_CROP_OPT: prepifg.CUSTOM_CROP, IFG_LKSX: 1, IFG_LKSY: 1}
-		xs = 0.000833333
-		ys = -xs
-		params[IFG_XFIRST] = 150.91 + (7 * xs)
-		params[IFG_YFIRST] = -34.17 + (16 * ys)
-		params[IFG_XLAST] = 150.9491667 + xs
-		params[IFG_YLAST] = -34.23 - (4 * xs)
-		params[IFG_FILE_LIST] = join(self.testdir, 'obs/ifms')
-		params[OBS_DIR] = join(self.testdir,"obs/")
-		
-		# output files should have same extents
+		params = self._custom_extents_param()
 		prepifg.prepare_ifgs(params)
 		ifg = Ifg(self.exp_files[0], self.hdr_files[0])
 		ifg.open()
 		gt = ifg.dataset.GetGeoTransform()
-		exp_gt = (params[IFG_XFIRST], xs, 0, params[IFG_YFIRST], 0, ys)
+		exp_gt = (params[IFG_XFIRST], self.xs, 0, params[IFG_YFIRST], 0, self.ys)
 		for i,j in zip(gt, exp_gt):
 			self.assertAlmostEqual(i, j)
-		assert_geotransform_equal(self.exp_files)		
+		assert_geotransform_equal(self.exp_files)
+		
+	
+	def test_custom_extents_misalignment(self):
+		"""Test misaligned cropping extents raise errors."""
+		
+		for key in [IFG_XFIRST, IFG_YFIRST, IFG_XLAST, IFG_YLAST]:
+			params = self._custom_extents_param() # reset params to prevent old params causing failure
+			backup = params[key]
+			
+			# try different errors for each var
+			for error in [0.1, 0.001, 0.0001, 0.00001, 0.000001]:
+				params[key] = backup + error
+				self.assertRaises(prepifg.PreprocessingException, prepifg.prepare_ifgs, params, use_exceptions=True)
 
 
 	def test_nodata(self):
