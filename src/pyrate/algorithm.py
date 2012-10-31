3d05bbc2e4694059bbbed38ee98404cddf8c03a7
Ben Davies <ben.davies@anu.edu.au>
2012-10-31 17:41:33 +1100

    Added function to calculate proportion of NaNs in an n-dimensional dataset.
    Unittest added.

--- src/pyrate/algorithm.py
+++ src/pyrate/algorithm.py
@@ -5,6 +5,8 @@ Author: Ben Davies
 
 from math import pi
 
+from numpy import nan, isnan, sum
+
 # constants
 MM_PER_METRE = 1000
 
@@ -12,3 +14,18 @@ MM_PER_METRE = 1000
 def wavelength_to_mm(data, wavelength):
 	"""Converts ROIPAC phase from metres to millimetres"""
 	return data * MM_PER_METRE * (wavelength / (4 * pi))
+
+
+def nan_fraction(data):
+	"""Calculates fraction of cells in data that are NaN"""
+	nan_mask = isnan(data)
+	nan_sum = sum(nan_mask)
+
+	# calc number of cells (sum additional dimensions as required)
+	shape = data.shape
+	ncells = float(shape[0])  # for float division
+	if len(shape) > 1:
+		for i in data.shape[1:]:
+			ncells *= i
+
+	return nan_sum / ncells
--- src/pyrate/algorithm_tests.py
+++ src/pyrate/algorithm_tests.py
@@ -3,18 +3,30 @@ import unittest
 from math import pi
 
 import numpy
+from numpy import nan
 
 import algorithm
 
 
 class AlgorithmTests(unittest.TestCase):
-	
+
 	def test_wavelength_conversion(self):
 		# ROIPAC is in metres, verify conversion to mm
 		xs, ys = 5,7
 		data = (numpy.arange(xs * ys) - 1.7) * 0.1 # fake a range of values
 		data = numpy.where(data == 0, numpy.nan, data)
 		wavelen = 0.0562356424
-		exp = (data * wavelen * 1000) / (4 * pi) 
+		exp = (data * wavelen * 1000) / (4 * pi)
 		act = algorithm.wavelength_to_mm(data, wavelen)
 		numpy.testing.assert_array_almost_equal(exp, act)
+
+
+	def test_nan_fraction(self):
+		data = numpy.ones((4,3))
+		data[1] = nan
+		exp = 3/ float(12)
+		act = algorithm.nan_fraction(data)
+		self.assertEqual(act, exp)
+
+
+
