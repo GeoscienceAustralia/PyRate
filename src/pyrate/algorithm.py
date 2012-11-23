b90e7b25afcb6bff948b69b0ca88fa6be451331c
Ben Davies <ben.davies@anu.edu.au>
2012-11-23 16:09:53 +1100

    Refactoring: broke down ref pixel search into functions for clarity.

--- src/pyrate/algorithm.py
+++ src/pyrate/algorithm.py
@@ -71,10 +71,6 @@ def get_epochs(ifgs):
 def ref_pixel(params, ifgs):
 	'''Return (y,x) reference pixel coordinate given open Ifgs.'''
 
-	def missing_option_error(option):
-		msg = "Missing '%s' in configuration options" % option
-		raise config.ConfigException(msg)
-
 	head = ifgs[0]
 	refx = params.get(config.REFX, 0)
 	refy = params.get(config.REFY, 0)
@@ -85,7 +81,44 @@ def ref_pixel(params, ifgs):
 			raise ValueError("Invalid reference pixel X coordinate: %s" % refx)
 		if refy < 1 or refy > head.FILE_LENGTH - 1:
 			raise ValueError("Invalid reference pixel Y coordinate: %s" % refy)
-		return (refy, refx)
+		return (refy, refx)  # reuse preset ref pixel
+
+	check_ref_pixel_params(params, head)
+
+	# pre-calculate useful amounts
+	refnx = params[config.REFNX]
+	refny = params[config.REFNY]
+	chipsize = params[config.REF_CHIP_SIZE]
+	radius = chipsize / 2
+	phase_stack = array([i.phase_data for i in ifgs]) # TODO: mem efficiencies?
+	thresh = params[config.REF_MIN_FRAC] * chipsize * chipsize
+	min_sd = float("inf") # dummy start value
+
+	# do window searches across dataset, central pixel of stack with smallest mean
+	# is the reference pixel
+	for y in _step(head.FILE_LENGTH, refny, radius):
+		for x in _step(head.WIDTH, refnx, radius):
+			data = phase_stack[:, y-radius:y+radius+1, x-radius:x+radius+1]
+			valid = [nsum(~isnan(i)) > thresh for i in data]
+
+			if all(valid): # ignore stack if 1+ ifgs have too many incoherent cells
+				sd = [std( i[~isnan(i)] ) for i in data]
+				mean_sd = mean(sd)
+				if mean_sd < min_sd:
+					min_sd = mean_sd
+					refy, refx = y, x
+
+	if (refy, refx) == (0, 0):
+		raise ReferencePixelException("Could not find a reference pixel")
+	return refy, refx
+
+
+def check_ref_pixel_params(params, head):
+	'''Validates reference pixel search parameters. head is any Ifg.'''
+
+	def missing_option_error(option):
+		msg = "Missing '%s' in configuration options" % option
+		raise config.ConfigException(msg)
 
 	# sanity check chipsize setting
 	chipsize = params.get(config.REF_CHIP_SIZE)
@@ -119,30 +152,6 @@ def ref_pixel(params, ifgs):
 	max_rows = (head.FILE_LENGTH - (chipsize-1))
 	if refny < 1 or refny > max_rows:
 		raise ValueError("Invalid refny setting, must be > 0 and < %s" % max_rows)
-
-	# pre-calculate useful amounts
-	radius = chipsize / 2
-	phase_stack = array([i.phase_data for i in ifgs]) # TODO: mem efficiencies?
-	thresh = min_frac * chipsize * chipsize
-	min_sd = float("inf") # dummy start value
-
-	# do window searches across dataset, central pixel of stack with smallest mean
-	# is the reference pixel
-	for y in _step(head.FILE_LENGTH, refny, radius):
-		for x in _step(head.WIDTH, refnx, radius):
-			data = phase_stack[:, y-radius:y+radius+1, x-radius:x+radius+1]
-			valid = [nsum(~isnan(i)) > thresh for i in data]
-
-			if all(valid): # ignore stack if 1+ ifgs have too many incoherent cells
-				sd = [std( i[~isnan(i)] ) for i in data]
-				mean_sd = mean(sd)
-				if mean_sd < min_sd:
-					min_sd = mean_sd
-					refy, refx = y, x
-
-	if (refy, refx) == (0, 0):
-		raise ReferencePixelException("Could not find a reference pixel")
-	return refy, refx
 
 
 def _step(dim, ref, radius):
