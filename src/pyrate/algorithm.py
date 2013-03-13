634723d848ed080dd6f326258f806dae04a30677
Ben Davies <ben.davies@anu.edu.au>
2013-03-13 17:08:30 +1100

    Added functionality to convert grid cell sizes in degrees to distance in metres.

--- src/pyrate/algorithm.py
+++ src/pyrate/algorithm.py
@@ -3,8 +3,9 @@ Collection of algorithms for PyRate.
 Author: Ben Davies, ANUSF
 '''
 
-from math import pi
+from math import pi, floor
 from numpy import sin, cos, unique, histogram
+import pyproj
 
 from shared import EpochList
 
@@ -55,10 +56,7 @@ def ifg_date_lookup(ifgs, date_pair):
 
 def get_epochs(ifgs):
 	'''Returns an EpochList derived from all given interferograms.'''
-	masters = [i.MASTER for i in ifgs]
-	slaves = [i.SLAVE for i in ifgs]
-
-	combined = masters + slaves
+	combined = [i.MASTER for i in ifgs] + [i.SLAVE for i in ifgs]
 	dates, n = unique(combined, False, True)
 	repeat, _ = histogram(n, bins=len(set(n)))
 
@@ -68,7 +66,40 @@ def get_epochs(ifgs):
 
 
 def master_slave_ids(dates):
-	'''Returns dict of 'date:unique ID' for each date in dates. IDs are sorted in
-	oldest to newest date order, starting at 0.'''
+	'''Returns dict of 'date:unique ID' for each date in dates.
+	IDs are sorted in	oldest to newest date order, starting at 0.
+	'''
 	dset = sorted(set(dates))
 	return dict([(date_, i) for i, date_ in enumerate(dset)])
+
+
+def utm_zone(longitude):
+	'''Returns basic UTM zone for given longitude in degrees. Currently does NOT
+	handle the sub-zoning around Scandanavian countries.
+	See http://www.dmap.co.uk/utmworld.htm
+	'''
+	if longitude == 180:
+		return 60.0
+	return floor((longitude + 180)/6.0) + 1
+
+
+def cell_size(lat, lon, x_step, y_step):
+	'''Converts X|Y_STEP in degrees to X & Y cell length/width in metres.
+	lat - latitude in degrees
+	lon - longitude in degrees
+	x_step - horizontal step size in degrees
+	y_step - vertical step size in degrees
+	'''
+	if lat > 84.0 or lat < -80:
+		msg = "No UTM zone for polar region: > 84 degrees N or < 80 degrees S"
+		raise ValueError(msg)
+
+	zone = utm_zone(lon)
+	p0 = pyproj.Proj(proj='latlong', ellps='WGS84')
+	p1 = pyproj.Proj(proj='utm', zone=zone, ellps='WGS84')
+	assert p0.is_latlong()
+	assert not p1.is_latlong()
+
+	x0, y0 = pyproj.transform(p0, p1, lon, lat)
+	x1, y1 = pyproj.transform(p0, p1, lon + x_step, lat + y_step)
+	return (abs(e) for e in (x1 - x0, y1 - y0))
