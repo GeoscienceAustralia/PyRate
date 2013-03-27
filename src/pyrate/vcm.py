b8f5a029c0cde1f6268335c067b0ffc2ee1981a9
Ben Davies <ben.davies@anu.edu.au>
2013-03-27 13:24:29 +1100

    Bugfix: cleaned up args to fmin().

--- src/pyrate/vcm.py
+++ src/pyrate/vcm.py
@@ -17,16 +17,19 @@
 
 from copy import copy
 from numpy import array, where, isnan, real, imag, sum, sqrt, meshgrid, reshape
-from numpy import zeros, vstack, ceil, mean
+from numpy import zeros, vstack, ceil, mean, exp
 from numpy.linalg import norm
 from scipy.fftpack import fft2, ifft2, fftshift
 from scipy.optimize import fmin
 
 
 def pendiffexp(alphamod, cvdav):
-	'''TODO exp covariance model'''
-	a = cvdav[0,1] #maxvar
-	return norm(cvdav[:,1] - a * exp(-alphamod*cvdav[:,0]))
+	'''TODO exp covariance model
+	TODO: returns a float of magnitude of the residuals
+	alphamod - TODO
+	cvdav - 2 col array of radius, variance (can be plotted as X,Y line)'''
+	mx = cvdav[:,1].max()
+	return norm(cvdav[:,1] - (mx * exp(-alphamod * cvdav[:,0])))
 
 
 def unique_points(points):
@@ -34,7 +37,6 @@ def unique_points(points):
 	points - sequence of (y,x) or (x,y) tuples
 	'''
 	return vstack([array(u) for u in set(points) ] )
-
 
 
 def cvd(ifg):
@@ -63,7 +65,7 @@ def cvd(ifg):
 
 	#	TODO: the line below is a a method to remove duplicate cells
 	#r = r[:ceil(len(r)/2)] # BUG comment? only need 1st half of image (symmetry)
-	# NB: old one had '+nlines', reason unknow, MG says ignore it.
+	# NB: old one had '+nlines', reason unknown, MG says ignore it.
 
 	#acg = acg[:len(r)]
 
@@ -73,7 +75,7 @@ def cvd(ifg):
 	#print 'r',r
 
 	acgorig = copy(acg)
-	maxvar=max(acg[:len(r)])
+	maxvar = max(acg[:len(r)])
 
 	# pick the smallest axis to focus on a circle around the centre point????
 	#print 'ifg.X_CENTRE, ifg.Y_CENTRE=', ifg.X_CENTRE, ifg.Y_CENTRE
@@ -81,14 +83,11 @@ def cvd(ifg):
 	if (ifg.X_CENTRE * ifg.X_SIZE) < (ifg.Y_CENTRE * ifg.Y_SIZE):
 		maxdist = ifg.X_CENTRE * ifg.X_SIZE
 		w = ifg.X_SIZE * 2  # bin width
-		print "maxdist from X axis"
+		#print "maxdist from X axis is", maxdist
 	else:
 		maxdist = ifg.Y_CENTRE * ifg.Y_SIZE
 		w = ifg.Y_SIZE * 2  # bin width
-		print "maxdist from Y axis"
-
-	#print 'maxdist=', maxdist
-
+		#print "maxdist from Y axis is", maxdist
 
 	#rsub = array([e for e in r if e < maxdist]) # MG: prefers to use all the data
 	rsub = r
@@ -104,13 +103,15 @@ def cvd(ifg):
 	print 'maxbin',maxbin
 
 	for b in range(maxbin):
-		cvdav[1,b] = mean(acg[rbin == b])
-		cvdav[0,b] = b * w
+		cvdav[0,b] = b * w # distance instead of bin #
+		cvdav[1,b] = mean(acg[rbin == b]) # mean variance for that bin
 
 	print 'cvdav',cvdav
 
 	# calculate best fit function maxvar*exp(-alpha*r)
 	#alpha = fmin(pendiffexp, [2 / (maxbin * w)], [], cvdav)
-	alpha = fmin(pendiffexp, None, args=([2 / (maxbin * w)], cvdav)  )
+	alphaguess = 2 / (maxbin * w)
+	alpha = fmin(pendiffexp, x0=alphaguess, args=(cvdav,) )
+	print "1st guess, alpha", alphaguess, alpha
 	
 	return maxvar, alpha
