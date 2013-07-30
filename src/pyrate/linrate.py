79f53eae98904a7b1e2cd00609f79a1d63230008
Ben Davies <ben.davies@anu.edu.au>
2013-07-30 16:06:11 +1000

    Added linear rate stubs.

---
+++ src/pyrate/linrate.py
@@ -0,0 +1,10 @@
+'''
+Pixel-by-pixel linear rate estimation using iterative least-squares method.
+Rate is equivalent to velocity.
+
+Author: Ben Davies, ANUSF
+'''
+
+
+def linear_rate(ifgs, vcm, params, mst, cbasep=None):
+	raise NotImplementedError
---
+++ src/pyrate/linrate_tests.py
@@ -0,0 +1,30 @@
+'''
+Unittests for linrate.py
+Author: Ben Davies, ANUSF
+'''
+
+import unittest
+
+
+# TODO:
+# 1. replace MST key:value date:date pairs with lists of Ifgs?
+# 2.: MG: fake some data for a 1 pixel test of stack()
+#     figure out what partial inputs we need to do this as a simple test
+# 3. MG is going to check if someome at GA has coded lscov in Py (or C/C++/Fortran??)
+#
+# MG thinks we'll need:
+# 1 all the func args
+# 2 a VCM
+# 3 the time spans (already in Ifgs?)
+# 4 the ifg displacement obs (phase_data)
+
+
+
+class LinearRateTests(unittest.TestCase):
+
+	def test_stack_basic(self):
+		raise NotImplementedError
+
+
+	def test_args(self):
+		raise NotImplementedError("Need sanity tests for args to stack()")
