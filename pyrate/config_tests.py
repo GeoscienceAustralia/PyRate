c2f4f7bb291d795547ee48a705c6cb78501fd1e3
Ben Davies <ben.davies@anu.edu.au>
2014-05-16 14:27:50 +1000

    Replaced file paths with constants.

--- pyrate/config_tests.py
+++ pyrate/config_tests.py
@@ -4,23 +4,23 @@ Created on 17/09/2012
 '''
 
 import sys, unittest
-from os.path import exists
+from os.path import exists, join
 
 import config
-
+from tests_common import SYD_TEST_DIR, SYD_TEST_OBS
 
 
 class ConfigTests(unittest.TestCase):
 
 	def __init__(self, *args, **kwargs):
 		super(ConfigTests, self).__init__(*args, **kwargs)
-		if not exists("../../tests/sydney_test/obs"):
-			sys.exit("ERROR: Missing the sydney_test data for unittests\n")
+		if not exists(SYD_TEST_OBS):
+			sys.exit("ERROR: Missing sydney_test data for unit tests\n")
 
 	# TODO: are more conf parsing tests needed?
 
 	def test_read_param_file(self):
-		conf_file = "../../tests/sydney_test/pyrate.conf"
+		conf_file = join(SYD_TEST_DIR, 'pyrate.conf')
 		params = config.parse_conf_file(conf_file)  # (params_file)
 		for k in params.keys():
 			self.assertTrue(k != "")
@@ -29,12 +29,12 @@ class ConfigTests(unittest.TestCase):
 
 
 	def test_parse_namelist(self):
-		nl = "../../tests/sydney_test/obs/ifms_17"
+		nl = join(SYD_TEST_OBS, 'ifms_17')
 		result = config.parse_namelist(nl)
 		self.assertEqual(len(result), 17)
 		files = ["geo_060619-061002.unw", "geo_060828-061211.unw",
-						"geo_061002-070430.unw", "geo_070115-070917.unw",
-						"geo_070219-070604.unw" ]
+					"geo_061002-070430.unw", "geo_070115-070917.unw",
+					"geo_070219-070604.unw" ]
 
 		for path in files:
 			self.assertTrue(path in result)
