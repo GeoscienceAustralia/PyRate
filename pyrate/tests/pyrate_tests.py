2327244af94eca1e1344ca4619d076803eb50146
Ben Davies <ben.davies@anu.edu.au>
2014-06-03 12:55:09 +1000

    Commented out test stub to prevent error report.

--- pyrate/tests/pyrate_tests.py
+++ pyrate/tests/pyrate_tests.py
@@ -3,17 +3,17 @@ Created on 17/09/2012
 @author: bpd900
 '''
 
-import os, unittest
+import unittest
 
 from pyrate import pyrate
 
 
 
-class PyRateTests(unittest.TestCase):
-
-	def test_pyrate_main(self):
-		#pyrate.main(cfgfile, verbose)
-		raise NotImplementedError
+# class PyRateTests(unittest.TestCase):
+# 
+# 	def test_pyrate_main(self):
+# 		# pyrate.main(cfgfile, verbose=False)
+# 		pass
 
 
 if __name__ == "__main__":
