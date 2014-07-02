c5e61c2fedfdf9c60dad292f4b31e1b0383ad455
Ben Davies <ben.davies@anu.edu.au>
2014-07-02 17:59:00 +1000

    Added basic init to workflow tests.

--- pyrate/tests/pyrate_tests.py
+++ pyrate/tests/pyrate_tests.py
@@ -1,20 +1,69 @@
 '''
+Tests end-to-end runs of the PyRate workflow module.
+
 Created on 17/09/2012
-@author: bpd900
+@author: Ben Davies, NCI
 '''
 
-import unittest
+import os
+from os.path import join
+import shutil
 
+import unittest
 from pyrate import pyrate
 
 
+# testing constants
+BASE_DIR = '/tmp/pyrate/workflow'
+BASE_TIF_DIR = join(BASE_DIR, 'tif')
+BASE_CFG_FILE = join(BASE_DIR, 'pyrate_workflow_test.conf')
 
-# class PyRateTests(unittest.TestCase):
-# 
-# 	def test_pyrate_main(self):
-# 		# pyrate.main(cfgfile, verbose=False)
-# 		pass
+TEST_CORE = join(os.environ['PYRATEPATH'], 'tests', 'sydney_test')
+
+CURRENT_DIR = os.getcwd()
+
+
+
+class PyRateTests(unittest.TestCase):
+	# initialise and run workflow from the class setup
+	# unit tests verify the different steps have completed
+
+	@classmethod
+	def setUpClass(cls):
+		try:
+			# assume outputs/working dir are part of same tree
+			# link the sydney data in
+			if not os.path.exists(BASE_DIR):
+				os.makedirs(BASE_DIR)
+
+			# link to config file and source data
+			if not os.path.exists(BASE_CFG_FILE):
+				orig_cfg = join(TEST_CORE, 'pyrate_workflow_test.conf')
+				os.symlink(orig_cfg, BASE_CFG_FILE)
+
+			if not os.path.exists(BASE_TIF_DIR):
+				orig_tif = join(TEST_CORE, 'tif')
+				os.symlink(orig_tif, BASE_TIF_DIR)
+
+			os.chdir(BASE_DIR)
+			pyrate.main(BASE_CFG_FILE, verbose=False)
+		except:
+			# get out to avoid paths busting other tests
+			os.chdir(CURRENT_DIR)
+			raise
+
+	@classmethod
+	def tearDownClass(cls):
+		shutil.rmtree(BASE_DIR)
+
+
+	def test_initial_setup(self):
+		self.assertTrue(os.path.exists(BASE_DIR))
+		self.assertTrue(os.path.exists(BASE_TIF_DIR))
+		self.assertTrue(os.path.exists(BASE_CFG_FILE))
+
 
 
 if __name__ == "__main__":
 	unittest.main()
+
