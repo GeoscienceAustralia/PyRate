5babf152d47d55532855fd282115b1797b4fc080
Ben Davies <ben.davies@anu.edu.au>
2014-08-22 13:01:12 +1000

    Minor cleanup/refactoring.

--- pyrate/tests/config_tests.py
+++ pyrate/tests/config_tests.py
@@ -12,42 +12,29 @@ from pyrate import config
 from common import SYD_TEST_DIR, SYD_TEST_TIF
 
 
-class ConfigTests(unittest.TestCase):
+def test_read_param_file():
+	conf_path = join(SYD_TEST_DIR, 'pyrate.conf')
+	params = config.get_config_params(conf_path)
+	for k in params.keys():
+		assert k and len(k) > 1
+		assert params[k] != ''
+		assert not k.endswith(":") # are the colons removed?
 
-	def __init__(self, *args, **kwargs):
-		super(ConfigTests, self).__init__(*args, **kwargs)
-		if not exists(SYD_TEST_TIF):
-			sys.exit("ERROR: Missing sydney_test data for unit tests\n")
+def test_read_param_file_no_refpixel():
+	# ensure the parser can handle empty fields
+	conf_path = join(SYD_TEST_DIR, 'pyrate2.conf')
+	params = config.get_config_params(conf_path)
 
-	# TODO: are more conf parsing tests needed?
+	assert params[config.REFX] == -1
+	assert params[config.REFY] == -1
 
-	def test_read_param_file(self):
-		conf_path = join(SYD_TEST_DIR, 'pyrate.conf')
-		params = config.get_config_params(conf_path)
-		for k in params.keys():
-			self.assertTrue(k != "")
-			self.assertTrue(params[k] != "")
-			self.assertFalse(k.endswith(":")) # are the colons removed?
+def test_parse_namelist():
+	nl = join(SYD_TEST_TIF, 'ifms_17')
+	result = config.parse_namelist(nl)
+	assert len(result) == 17
+	files = ["geo_060619-061002.tif", "geo_060828-061211.tif",
+				"geo_061002-070430.tif", "geo_070115-070917.tif",
+				"geo_070219-070604.tif" ]
 
-	def test_read_param_file_no_refpixel(self):
-		# ensure the parser can handle empty fields
-		conf_path = join(SYD_TEST_DIR, 'pyrate2.conf')
-		params = config.get_config_params(conf_path)
-
-		self.assertTrue(params[config.REFX] == -1)
-		self.assertTrue(params[config.REFY] == -1)
-
-	def test_parse_namelist(self):
-		nl = join(SYD_TEST_TIF, 'ifms_17')
-		result = config.parse_namelist(nl)
-		self.assertEqual(len(result), 17)
-		files = ["geo_060619-061002.tif", "geo_060828-061211.tif",
-					"geo_061002-070430.tif", "geo_070115-070917.tif",
-					"geo_070219-070604.tif" ]
-
-		for path in files:
-			self.assertTrue(path in result)
-
-
-if __name__ == "__main__":
-	unittest.main()
+	for path in files:
+		assert path in result
