fix Variance Covariance Matrix production and Covariance fitting function codes. 	- add two tests to verify algorithms against Matlab verbatim output

pyrate/vcm.py
@@ -2,7 +2,7 @@
 Variance/Covariance matrix functionality for PyRate, ported from Hua Wang's
 MATLAB code for Pirate.
 
-.. codeauthor:: Ben Davies
+.. codeauthor:: Ben Davies, Matt Garthwaite
 '''
 
 from copy import copy
@@ -15,8 +15,6 @@ from scipy.optimize import fmin
 from pyrate.algorithm import master_slave_ids
 
 
-# NB: Hua hardwired the original to only do the calculations spectrally
-
 def pendiffexp(alphamod, cvdav):
     '''
     Fits an exponential model to data.
@@ -25,8 +23,9 @@ def pendiffexp(alphamod, cvdav):
     :param array cvdav: Function magnitude at 0 radius (2 col array of radius, variance)
     '''
 
-    mx = cvdav[:,1].max()
-    return norm(cvdav[:,1] - (mx * exp(-alphamod * cvdav[:,0])))
+    # maxvar usually at zero lag
+    mx = cvdav[1,0]
+    return norm(cvdav[1,:] - (mx * exp(-alphamod * cvdav[0,:])))
 
 
 def unique_points(points):
@@ -41,40 +40,35 @@ def unique_points(points):
 
 def cvd(ifg):
     '''
-    Calculate average covariance versus distance (autocorrelation)
+    Calculate average covariance versus distance (autocorrelation) and its best fitting exponential function
 
     :param ifg: An interferogram.
     :type ifg: :py:class:`pyrate.shared.Ifg`.
     '''
+    # distance division factor of 1000 converts to km and is needed to match Matlab code output
+    distfact = 1000
 
+    # calculate 2D auto-correlation of image using the spectral method (Wiener-Khinchin theorem)
     phase = where(isnan(ifg.phase_data), 0, ifg.phase_data)
-
     fft_phase = fft2(phase)
     pspec = real(fft_phase)**2 + imag(fft_phase)**2
     autocorr_grid = ifft2(pspec)
-
     nzc = sum(sum(phase != 0))
     autocorr_grid = fftshift(real(autocorr_grid)) / nzc
-    acg = reshape(autocorr_grid, ifg.num_cells, 1)
-
+        
+    # pixel distances from pixel at zero lag (image centre).
     xx,yy = meshgrid(range(ifg.ncols), range(ifg.nrows))
-    r = sqrt(((xx-ifg.x_centre) * ifg.x_size)**2 + ((yy-ifg.y_centre) * ifg.y_size)**2)  # radius from centre???
-    r = reshape(r, ifg.num_cells) #, 1)
-
-    # ACG is on the Y axis, R is on the X axis
+    r = sqrt(((xx-ifg.x_centre) * ifg.x_size)**2 + ((yy-ifg.y_centre) * ifg.y_size)**2) / distfact
+  
+    r = reshape(r, ifg.num_cells, 1)
+    acg = reshape(autocorr_grid, ifg.num_cells, 1)    
+  
+    # Symmetry in image; keep only unique points
     tmp = unique_points(zip(acg, r))
     acg, r = tmp[:,0], tmp[:,1]
-
-    #print 'acg', acg
-    #print "/" * 40
-    #print 'r', r
-    #print "/" * 40
-
-    #    NB: the line below is a a method to remove duplicate cells
-    #r = r[:ceil(len(r)/2)] # BUG comment? only need 1st half of image (symmetry)
-    # NB: old one had '+nlines', reason unknown, MG says ignore it.
-
-    #acg = acg[:len(r)]
+    
+    # Alternative method to remove duplicate cells from Matlab Pirate
+    #r = r[:ceil(len(r)/2)+nlines] # Reason for '+nlines' term unknown
 
     # TODO: write a better way of getting rid of duplicate {r, acg} pairs - sets?
     # eg. array([x for x in set([(1,1), (2,2), (1,1)])])
@@ -82,41 +76,45 @@ def cvd(ifg):
     #print 'r',r
 
     acgorig = copy(acg)
-    maxvar = max(acg[:len(r)])
-
-    # pick the smallest axis to focus on a circle around the centre point????
-    #print 'ifg.X_CENTRE, ifg.Y_CENTRE=', ifg.X_CENTRE, ifg.Y_CENTRE
-    #print 'ifg.X_SIZE, ifg.Y_SIZE', ifg.X_SIZE, ifg.Y_SIZE
+    rorig = copy(r)
+    
+    # bin width for collecting data
+    w = max(ifg.x_size, ifg.y_size) * 2 / distfact
+    
+    # pick the smallest axis to determine circle search radius
+    #print 'ifg.X_CENTRE, ifg.Y_CENTRE=', ifg.x_centre, ifg.y_centre
+    #print 'ifg.X_SIZE, ifg.Y_SIZE', ifg.x_size, ifg.y_size
     if (ifg.x_centre * ifg.x_size) < (ifg.y_centre * ifg.y_size):
-        maxdist = ifg.x_centre * ifg.x_size
-        w = ifg.x_size * 2  # bin width
+        maxdist = ifg.x_centre * ifg.x_size / distfact
         #print "maxdist from X axis is", maxdist
     else:
-        maxdist = ifg.y_centre * ifg.y_size
-        w = ifg.y_size * 2  # bin width
+        maxdist = ifg.y_centre * ifg.y_size/ distfact
         #print "maxdist from Y axis is", maxdist
-
-    #rsub = array([e for e in r if e < maxdist]) # MG: prefers to use all the data
-    rsub = r
-    acg=acgorig;
-    #acg = [e for e in r if e < maxdist]
-
-    rbin = ceil(rsub / w).astype(int)  # classify values of r according to bin number
-
-    maxbin = max(rbin) + 1
+    
+    # filter out data where the of lag distance is greater than maxdist
+    #r = array([e for e in rorig if e <= maxdist]) # MG: prefers to use all the data
+    #acg = array([e for e in rorig if e <= maxdist])    
+    r = rorig[rorig<maxdist]
+    acg = acgorig[rorig<maxdist]
+       
+    # classify values of r according to bin number
+    rbin = ceil(r / w).astype(int)
+    maxbin = max(rbin) # consistent with Matlab code
+    
     cvdav = zeros( (2, maxbin) )
 
     for b in range(maxbin):
-        cvdav[0,b] = b * w # distance instead of bin #
+        cvdav[0,b] = b * w # distance instead of bin number
         cvdav[1,b] = mean(acg[rbin == b]) # mean variance for that bin
 
     # calculate best fit function maxvar*exp(-alpha*r)
-    #alpha = fmin(pendiffexp, [2 / (maxbin * w)], [], cvdav)
     alphaguess = 2 / (maxbin * w)
-    alpha = fmin(pendiffexp, x0=alphaguess, args=(cvdav,) )
+    alpha = fmin(pendiffexp, x0=alphaguess, args=(cvdav,), disp=0 )
     print "1st guess, alpha", alphaguess, alpha
 
     assert len(alpha) == 1
+    # maximum variance usually at the zero lag
+    maxvar = max(acg[:len(r)])
     return maxvar, alpha[0]
 
 
@@ -132,10 +130,10 @@ def get_vcmt(ifgs, maxvar):
     dates = [ifg.master for ifg in ifgs] + [ifg.slave for ifg in ifgs]
     ids = master_slave_ids(dates)
 
-    for i, ifg in enumerate(ifgs): # TODO: do we need -1?
+    for i, ifg in enumerate(ifgs):
         mas1, slv1 = ids[ifg.master], ids[ifg.slave]
 
-        for j, ifg2 in enumerate(ifgs): # TODO: do we need +1?
+        for j, ifg2 in enumerate(ifgs):
             mas2, slv2 = ids[ifg2.master], ids[ifg2.slave]
             if mas1 == mas2 or slv1 == slv2:
                 vcm_pat[i,j] = 0.5