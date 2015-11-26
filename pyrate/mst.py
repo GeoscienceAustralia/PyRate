c2427c954d5f6e2ad8aea1399889284be5d7b105
Sudipta Basak <basaks@gmail.com>
2015-11-26 15:07:25 +1100

    ported to `networkx` from `pygraph`. Passes mst tests. TODO: test performance with larger datafiles.

--- pyrate/mst.py
+++ pyrate/mst.py
@@ -8,13 +8,13 @@ Contains functions to calculate MST using interferograms.
 
 from itertools import product
 from numpy import array, nan, isnan, float32, empty
-import numpy as np
 
 from pyrate.algorithm import get_all_epochs, master_slave_ids, ifg_date_lookup, get_epochs
 from pyrate.algorithm import ifg_date_index_lookup
 
 from pygraph.classes.graph import graph
 from pygraph.algorithms.minmax import minimal_spanning_tree
+import networkx as nx
 
 # TODO: may need to implement memory saving row-by-row access
 # TODO: document weighting by either Nan fraction OR variance
@@ -29,6 +29,14 @@ def _remove_root_node(mst):
         if mst[k] is None:
             del mst[k]
     return mst
+
+
+def _remove_root_node_new(mst):
+    """
+    Discard pygraph's root nodes from MST dict to conserve memory.
+    Note multiple root nodes exist in MST dict.
+    """
+    return {k: v for k, v in mst.items() if v}
 
 
 # TODO: this does not appear to be used anywhere
@@ -58,8 +66,28 @@ def _build_graph(nodes, edges, weights, noroot=True):
     g.add_nodes(nodes)
     for edge, weight in zip(edges, weights):
         g.add_edge(edge, wt=weight)
-
     return g
+
+
+def _build_graph_networkx(nodes, edges_with_weights, noroot=True):
+    """
+    Convenience graph builder function: returns a new graph obj.
+    """
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_weighted_edges_from(edges_with_weights)
+    return g
+
+
+def _build_graph_new(nodes, edges, weights, noroot=True):
+    """
+    Convenience graph builder function: returns a new graph obj.
+    Equivalent performance to that of _build_graph for test data.
+    May be faster than _build_graph for larger ifgs.
+    """
+    g = graph()
+    g.add_nodes(nodes)
+    return map(lambda e, w: g.add_edge(edge=e, wt=w), edges, weights)
 
 
 def mst_matrix_ifgs_only(ifgs):
@@ -74,7 +102,7 @@ def mst_matrix_ifgs_only(ifgs):
 
     result = empty(shape=ifgs[0].phase_data.shape, dtype=object)
 
-    for y, x, mst in mst_matrix(ifgs):
+    for y, x, mst in mst_matrix_networkx(ifgs):
         # mst is a dictionary of dates
         if hasattr(mst, 'iteritems'):
             ifg_sub = [ifg_date_lookup(ifgs, d) for d in mst.iteritems()]
@@ -168,5 +196,48 @@ def mst_matrix(ifgs):
                 if g.has_edge(edge):
                     g.del_edge(edge)
 
-        mst = _remove_root_node(minimal_spanning_tree(g))
+        mst = _remove_root_node_new(minimal_spanning_tree(g))
         yield y, x, mst
+
+
+def mst_matrix_networkx(ifgs):
+    """
+    Generates/emits MST trees on a pixel-by-pixel basis for the given ifgs.
+    :param ifgs: sequence of Ifg objs
+    """
+
+    # make default MST to optimise result when no Ifg cells in a stack are nans
+    epochs = get_epochs(ifgs)
+    edges_with_weights_for_networkx = [(i.master, i.slave, i.nan_fraction) for i in ifgs]
+    g_nx = _build_graph_networkx(epochs.dates, edges_with_weights_for_networkx)
+    T = nx.minimum_spanning_tree(g_nx)
+
+    # TODO: memory efficiencies can be achieved here with tiling
+    data_stack = array([i.phase_data for i in ifgs], dtype=float32)
+
+    # create MSTs for each pixel in the ifg data stack
+    nifgs = len(ifgs)
+
+    for y, x in product(xrange(i.nrows), xrange(i.ncols)):
+        values = data_stack[:, y, x]  # vertical stack of ifg values for a pixel
+        nan_count = sum(isnan(values))
+
+        # optimisations: use pre-created results for all nans/no nans
+        if nan_count == 0:
+            yield y, x, T.edges()
+            continue
+        elif nan_count == nifgs:
+            yield y, x, nan
+            continue
+
+        # dynamically modify graph to reuse a single graph: this should avoid
+        # repeatedly creating new graph objs & reduce RAM use
+        for value, edge in zip(values, edges_with_weights_for_networkx):
+            if not isnan(value):
+                if not g_nx.has_edge(edge[0], edge[1]):
+                    g_nx.add_edge(edge[0], edge[1], weight=edge[2])
+            else:
+                if g_nx.has_edge(edge[0], edge[1]):
+                    g_nx.remove_edges_from(edge)
+
+        yield y, x, T.edges()
