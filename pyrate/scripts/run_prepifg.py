621ee0d932918fa5d0a3655db5647a8aa53af761
Sudipta Basak <basaks@gmail.com>
2016-01-12 10:49:08 +1100

    [run_prepifg] convenience variable introduced in main

--- pyrate/scripts/run_prepifg.py
+++ pyrate/scripts/run_prepifg.py
@@ -4,13 +4,24 @@ from pyrate.tasks.utils import pythonifyConfig
 from pyrate.tasks.prepifg import PrepareInterferograms
 
 
-def main():
-    usage = 'Usage: python run_prepifg.py <config file>'
-    if len(sys.argv) == 1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
-        print usage
-        return
-    rawConfigFile = sys.argv[1]
-    luigi.configuration.LuigiConfigParser.add_config_path(pythonifyConfig(rawConfigFile))
+def main(config_file=None):
+    """
+    :param config_file: config file to use. This provides a convenient way to
+     use run_prepifg from within the module.
+    :return:
+    """
+    if config_file:
+        print config_file
+        rawConfigFile = config_file
+    else:
+        usage = 'Usage: python run_prepifg.py <config file>'
+        if len(sys.argv) == 1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            print usage
+            return
+        rawConfigFile = sys.argv[1]
+
+    luigi.configuration.LuigiConfigParser.add_config_path(
+        pythonifyConfig(rawConfigFile))
     luigi.build([PrepareInterferograms()], local_scheduler=True)
 
 
