a60d509635168fd746152e818343ef7f90e17c5a
Sudipta Basak <basaks@gmail.com>
2016-04-05 11:56:32 +1000

    consistent naming of test directories

--- pyrate/tests/common.py
+++ pyrate/tests/common.py
@@ -20,7 +20,7 @@ SYD_TEST_OUT = join(SYD_TEST_DIR, 'out')
 SYD_TEST_TIF = join(SYD_TEST_DIR, 'tif')
 
 SYD_TEST_DEM_DIR = join(SYD_TEST_DIR, 'dem')
-SYD_TEST_MATLAB_MST_DIR = join(SYD_TEST_DIR, 'mst_matlab')
+SYD_TEST_MATLAB_MST_DIR = join(SYD_TEST_DIR, 'matlab_mst')
 SYD_TEST_MATLAB_PREPIFG_DIR = join(SYD_TEST_DIR, 'matlab_preifg_output')
 SYD_TEST_MATLAB_ORBITAL_DIR = join(SYD_TEST_DIR,
                                    'matlab_orbital_error_correction')
