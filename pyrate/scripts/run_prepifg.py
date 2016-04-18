f9216355f8ceac06ff1113231a514fb5700ca7da
Sudipta Basak <basaks@gmail.com>
2016-04-19 08:49:35 +1000

    refactored serial prepifg calls

--- pyrate/scripts/run_prepifg.py
+++ pyrate/scripts/run_prepifg.py
@@ -42,65 +42,73 @@ def main(params=None):
     if LUIGI:
         msg = "running luigi prepifg"
         print msg
-        logging.debug(msg)
+        logging.info(msg)
         luigi.configuration.LuigiConfigParser.add_config_path(
             pythonifyConfig(raw_config_file))
         luigi.build([PrepareInterferograms()], local_scheduler=True)
     else:
         msg = "running serial prepifg"
         print msg
-        logging.debug(msg)
-        xlooks, ylooks, crop = run_pyrate.transform_params(params)
-
+        logging.info(msg)
         if PROCESSOR == 0:
-            msg = "running roipac prepifg"
-            print msg
-            logging.debug(msg)
-            dem_file = os.path.join(params[cf.ROIPAC_RESOURCE_HEADER])
-            projection = roipac.parse_header(dem_file)[ifc.PYRATE_DATUM]
-            dest_base_ifgs = [os.path.join(
-                params[cf.OUT_DIR], os.path.basename(q).split('.')[0] + '.tif')
-                          for q in base_ifg_paths]
-
-            for b, d in zip(base_ifg_paths, dest_base_ifgs):
-                header_file = "%s.%s" % (b, ROI_PAC_HEADER_FILE_EXT)
-                header = roipac.parse_header(header_file)
-
-                # DEM already has DATUM, so get it from dem if not in header
-                if ifc.PYRATE_DATUM not in header:
-                    header[ifc.PYRATE_DATUM] = projection
-                roipac.to_geotiff(header, b, d, nodata=params[cf.NO_DATA_VALUE])
-            ifgs = [Ifg(p) for p in dest_base_ifgs]
-            prepifg.prepare_ifgs(
-                ifgs, crop_opt=crop, xlooks=xlooks, ylooks=ylooks)
+            roipac_prepifg(base_ifg_paths, params)
         else:
-            msg = "running gamma prepifg"
-            print msg
-            logging.debug(msg)
-            dem_hdr_path = params[cf.DEM_HEADER_FILE]
-            DEM_HDR = gamma.parse_dem_header(dem_hdr_path)
-            try:
-                SLC_DIR = params[cf.SLC_DIR]
-            except:
-                SLC_DIR = None
+            gamma_prepifg(base_ifg_paths, params)
 
-            # location of geo_tif's
-            dest_base_ifgs = [os.path.join(
-                params[cf.OUT_DIR], os.path.basename(q).split('.')[0] + '.tif')
-                          for q in base_ifg_paths]
 
-            for b, d in zip(base_ifg_paths, dest_base_ifgs):
-                header_paths = gamma_task.get_header_paths(b, slc_dir=SLC_DIR)
-                if len(header_paths) != 2:
-                    raise
-                hdrs = [gamma.parse_epoch_header(p) for p in header_paths]
-                COMBINED = gamma.combine_headers(hdrs[0], hdrs[1], dem_hdr=DEM_HDR)
-                gamma.to_geotiff(COMBINED, b, d,
-                                 nodata=params[cf.NO_DATA_VALUE])
+def roipac_prepifg(base_ifg_paths, params):
+    msg = "running roipac prepifg"
+    print msg
+    logging.info(msg)
+    xlooks, ylooks, crop = run_pyrate.transform_params(params)
+    dem_file = os.path.join(params[cf.ROIPAC_RESOURCE_HEADER])
+    projection = roipac.parse_header(dem_file)[ifc.PYRATE_DATUM]
+    dest_base_ifgs = [os.path.join(
+        params[cf.OUT_DIR], os.path.basename(q).split('.')[0] + '.tif')
+                      for q in base_ifg_paths]
+    for b, d in zip(base_ifg_paths, dest_base_ifgs):
+        header_file = "%s.%s" % (b, ROI_PAC_HEADER_FILE_EXT)
+        header = roipac.parse_header(header_file)
 
-            ifgs = [Ifg(p) for p in dest_base_ifgs]
-            prepifg.prepare_ifgs(
-                ifgs, crop_opt=crop, xlooks=xlooks, ylooks=ylooks)
+        # DEM already has DATUM, so get it from dem if not in header
+        if ifc.PYRATE_DATUM not in header:
+            header[ifc.PYRATE_DATUM] = projection
+        roipac.to_geotiff(header, b, d, nodata=params[cf.NO_DATA_VALUE])
+    ifgs = [Ifg(p) for p in dest_base_ifgs]
+    prepifg.prepare_ifgs(
+        ifgs, crop_opt=crop, xlooks=xlooks, ylooks=ylooks)
+
+
+def gamma_prepifg(base_ifg_paths, params):
+    msg = "running gamma prepifg"
+    print msg
+    logging.info(msg)
+    xlooks, ylooks, crop = run_pyrate.transform_params(params)
+    dem_hdr_path = params[cf.DEM_HEADER_FILE]
+    DEM_HDR = gamma.parse_dem_header(dem_hdr_path)
+    try:
+        SLC_DIR = params[cf.SLC_DIR]
+    except:
+        SLC_DIR = None
+
+    # location of geo_tif's
+    dest_base_ifgs = [os.path.join(
+        params[cf.OUT_DIR], os.path.basename(q).split('.')[0] + '.tif')
+                      for q in base_ifg_paths]
+
+    for b, d in zip(base_ifg_paths, dest_base_ifgs):
+        header_paths = gamma_task.get_header_paths(b, slc_dir=SLC_DIR)
+        if len(header_paths) != 2:
+            raise
+        hdrs = [gamma.parse_epoch_header(p) for p in header_paths]
+        COMBINED = gamma.combine_headers(hdrs[0], hdrs[1], dem_hdr=DEM_HDR)
+        gamma.to_geotiff(COMBINED, b, d,
+                         nodata=params[cf.NO_DATA_VALUE])
+
+    ifgs = [Ifg(p) for p in dest_base_ifgs]
+
+    prepifg.prepare_ifgs(
+        ifgs, crop_opt=crop, xlooks=xlooks, ylooks=ylooks)
 
 
 if __name__ == '__main__':
