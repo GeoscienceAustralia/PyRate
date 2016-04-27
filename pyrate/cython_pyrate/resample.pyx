8890c83d24d856c744c5b66c30bb11bec5c26e70
Sudipta Basak <basaks@gmail.com>
2016-04-27 11:32:57 +1000

    a bit more optimization

--- pyrate/cython_pyrate/resample.pyx
+++ pyrate/cython_pyrate/resample.pyx
@@ -1,15 +1,13 @@
 from __future__ import division
 import numpy as np
-cimport numpy as np
+cimport numpy as cnp
 cimport cython
-DTYPE = np.float32
-ctypedef np.float32_t DTYPE_t
 
 cdef greater(float x, float y):
     return x > y
 
 @cython.boundscheck(False)
-def resample(np.ndarray[DTYPE_t, ndim=2] data, int xscale, int yscale, double thresh):
+def resample(cnp.ndarray[cnp.float32_t, ndim=2] data, int xscale, int yscale, double thresh):
     """
     Resamples/averages 'data' to return an array from the averaging of blocks
     of several tiles in 'data'. NB: Assumes incoherent cells are NaNs.
@@ -20,26 +18,25 @@ def resample(np.ndarray[DTYPE_t, ndim=2] data, int xscale, int yscale, double th
     :param thresh: minimum allowable proportion of NaN cells (range from 0.0-1.0),
         eg. 0.25 = 1/4 or more as NaNs results in a NaN value for the output cell.
     """
-    cdef unsigned int x, y
-    cdef unsigned int ysize = data.shape[0]
-    cdef unsigned int xsize = data.shape[1]
+    cdef unsigned int x, y, ys, xs
+    cdef unsigned int ysize = data.shape[0], xsize = data.shape[1]
     cdef int tile_cell_count, xres, yres
-    cdef DTYPE_t nan_fraction
-    cdef np.ndarray[DTYPE_t, ndim=2] dest, tile
-
-    xres = int(xsize / xscale)
-    yres = int(ysize / yscale)
+    cdef cnp.float32_t nan_fraction
+    cdef cnp.ndarray[cnp.float32_t, ndim=2] dest, tile
+    xres = np.int(xsize / xscale)
+    yres = np.int(ysize / yscale)
     dest = np.zeros((yres, xres), dtype=np.float32) * np.nan
     tile_cell_count = xscale * yscale
+    thresh_bool = thresh == 0
 
     # calc mean without nans (fractional threshold ignores tiles with excess NaNs)
-    # for y, x in itertools.product(xrange(yres), xrange(xres)):
     for y in range(yres):
         for x in range(xres):
-            tile = data[y * yscale: (y+1) * yscale, x * xscale: (x+1) * xscale]
+            ys = y * yscale
+            xs = x * xscale
+            tile = data[ys: ys + yscale, xs: xs + xscale]
             nan_fraction = np.sum(np.isnan(tile)) / tile_cell_count
-
-            if greater(thresh, nan_fraction) or (nan_fraction == 0 and thresh == 0):
+            if greater(thresh, nan_fraction) or (nan_fraction == 0 and thresh_bool):
                 dest[y, x] = np.nanmean(tile)
 
     return dest
