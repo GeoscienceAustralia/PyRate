83a0ba31f7c0708824f1760ea8df6627ffd5f7a4
Sudipta Basak <basaks@gmail.com>
2016-05-11 10:32:41 +1000

    refactored mpi_mst_calc

--- pyrate/nci/run_pyrate_pypar.py
+++ pyrate/nci/run_pyrate_pypar.py
@@ -20,9 +20,7 @@ from pyrate import mst
 MASTER_PROCESS = 0
 
 
-def main():
-    clean_up_old_files()
-
+def main(params=None):
     # Setting up parallelisation
     parallel = Parallel(True)
     MPI_myID = parallel.rank
@@ -32,14 +30,20 @@ def main():
         print "Master process found {} worker processors".format(num_processors)
 
     # Read config file, cropped_and_sampled_tifs are input files to run_pyrate
-    _, cropped_and_sampled_tifs, params = run_pyrate.get_ifg_paths()
+    if params:
+        xlks, ylks, crop = run_pyrate.transform_params(params)
+        base_unw_paths = run_pyrate.original_ifg_paths(params[cf.IFG_FILE_LIST])
+        cropped_and_sampled_tifs = \
+            run_pyrate.get_dest_paths(base_unw_paths, crop, params, xlks)
+    else:
+        _, cropped_and_sampled_tifs, params = run_pyrate.get_ifg_paths()
+
 
     output_dir = params[cf.OUT_DIR]
     mpi_log_filename = os.path.join(output_dir, "mpi_run_pyrate.log")
 
     ### Master Process ###
     if MPI_myID == MASTER_PROCESS:
-        run_prepifg.main(params)  # create files again
         output_log_file = open(mpi_log_filename, "w")
         config_filepath = sys.argv[1]
         configfile = open(config_filepath)
@@ -62,40 +66,45 @@ def main():
         output_log_file.close()
 
     parallel.barrier()
+
+    # Calc mst using MPI
+    if MPI_myID == MASTER_PROCESS:
+        mst = mpi_mst_calc(MPI_myID, cropped_and_sampled_tifs, mpi_log_filename,
+                 num_processors, parallel, params)
+    else:
+        mpi_mst_calc(MPI_myID, cropped_and_sampled_tifs, mpi_log_filename,
+                 num_processors, parallel, params)
+
+    parallel.finalize()
+
+
+def mpi_mst_calc(MPI_myID, cropped_and_sampled_tifs, mpi_log_filename,
+                 num_processors, parallel, params):
     ifgs = run_pyrate.prepare_ifgs_for_networkx_mst(cropped_and_sampled_tifs,
                                                     params)
     top_lefts, bottom_rights, no_tiles = mst.setup_tiles(
-            ifgs[0].shape, processes=num_processors)
-
-
+        ifgs[0].shape, processes=num_processors)
     # parallel.calc_indices(no_tiles)
     process_indices = parallel.calc_indices(no_tiles)
-
     process_top_lefts = [itemgetter(p)(top_lefts)
-                          for p in process_indices]
-
+                         for p in process_indices]
     process_bottom_rights = [itemgetter(p)(bottom_rights)
-                            for p in process_indices]
-
+                             for p in process_indices]
     print 'Processor {mpi_id} has {processes} ' \
           'tiles out of {num_files}'.format(mpi_id=MPI_myID,
                                             processes=len(process_indices),
                                             num_files=no_tiles)
-
     result_process = mst.mst_multiprocessing_map(
         process_top_lefts, process_bottom_rights,
         cropped_and_sampled_tifs, ifgs[0].shape, no_ifgs=len(ifgs)
     )
-
     parallel.barrier()
-
     # send the result arrays
     if MPI_myID != MASTER_PROCESS:
         parallel.send(result_process, destination=MASTER_PROCESS, tag=MPI_myID)
         print "sent result from process", MPI_myID
 
     # parallel.barrier()
-
     if MPI_myID == MASTER_PROCESS:
         result = result_process
         # combine the mst from the other processes
@@ -104,13 +113,10 @@ def main():
                 parallel.receive(source=i, tag=-1, return_status=False)
             result += result_remote_processes
 
-        original_mst = mst.mst_boolean_array(ifgs)
-        np.testing.assert_array_equal(original_mst, result)
         output_log_file = open(mpi_log_filename, "a")
         output_log_file.write("\n\n Mst caclulation finished\n")
         output_log_file.close()
-
-    parallel.finalize()
+        return result
 
 
 def clean_up_old_files():
