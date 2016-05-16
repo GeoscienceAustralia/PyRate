8d81c9266c607c56110d78f3f57c0876032c45a4
Sudipta Basak <basaks@gmail.com>
2016-05-16 14:34:39 +1000

    [ref_phase] now can use multiprocessing on method 2

--- pyrate/reference_phase_estimation.py
+++ pyrate/reference_phase_estimation.py
@@ -21,7 +21,6 @@ def estimate_ref_phase(ifgs, params, refpx, refpy):
         :ifgs: reference phase data removed list of ifgs
     """
     number_ifgs = len(ifgs)
-    ref_phs = np.zeros(number_ifgs)
     _validate_ifgs(ifgs)
 
     # set reference phase as the average of the whole image (recommended)
@@ -29,28 +28,52 @@ def estimate_ref_phase(ifgs, params, refpx, refpy):
         ref_phs = est_ref_phase_method1(ifgs, params)
 
     elif int(params[cf.REF_EST_METHOD]) == 2:
-        half_chip_size = int(np.floor(params[cf.REF_CHIP_SIZE]/2.0))
-        chipsize = 2 * half_chip_size + 1
-        thresh = chipsize*chipsize*params[cf.REF_MIN_FRAC]
-        for n, i in enumerate(ifgs):
-            patch = i.phase_data[
-                    refpy - half_chip_size: refpy + half_chip_size + 1,
-                    refpx - half_chip_size: refpx + half_chip_size + 1
-                    ]
-            patch = np.reshape(patch, newshape=(-1, 1), order='F')
-            if np.sum(~np.isnan(patch)) < thresh:
-                raise ReferencePhaseError('The reference pixel'
-                                          'is not in high coherent area!')
-            ref_phs[n] = nanmedian(patch)
-            i.phase_data -= ref_phs[n]
+        ref_phs = est_ref_phase_method2(ifgs, params, refpx, refpy)
     else:
         raise ReferencePhaseError('No such option. Use refest=1 or 2')
 
     return ref_phs, ifgs
 
 
+def est_ref_phase_method2(ifgs, params, refpx, refpy):
+    ref_phs = np.zeros(len(ifgs))
+    half_chip_size = int(np.floor(params[cf.REF_CHIP_SIZE] / 2.0))
+    chipsize = 2 * half_chip_size + 1
+    thresh = chipsize * chipsize * params[cf.REF_MIN_FRAC]
+    phase_data = [i.phase_data for i in ifgs]
+    if params[cf.PARALLEL]:
+        ref_phs_ret = parmap.map(est_ref_phase_method2_milti, phase_data,
+                                 half_chip_size, refpx, refpy, thresh)
+        for n, ifg in enumerate(ifgs):
+            ref_phs[n] = ref_phs_ret[n][0]
+            ifg.phase_data = ref_phs_ret[n][1]
+    else:
+        ref_phs = np.zeros(len(ifgs))
+        for n, ifg in enumerate(ifgs):
+            ref_phs[n], ifg.phase_data = \
+                est_ref_phase_method2_milti(phase_data[n], half_chip_size,
+                                            refpx, refpy, thresh)
+    return ref_phs
+
+
+def est_ref_phase_method2_milti(phase_data, half_chip_size,
+                                refpx, refpy, thresh):
+    patch = phase_data[
+            refpy - half_chip_size: refpy + half_chip_size + 1,
+            refpx - half_chip_size: refpx + half_chip_size + 1
+            ]
+    patch = np.reshape(patch, newshape=(-1, 1), order='F')
+    if np.sum(~np.isnan(patch)) < thresh:
+        raise ReferencePhaseError('The reference pixel'
+                                  'is not in high coherent area!')
+    ref_ph = nanmedian(patch)
+    phase_data -= ref_ph
+    return ref_ph, phase_data
+
+
 def est_ref_phase_method1(ifgs, params):
     ifg_phase_data_sum = np.zeros(ifgs[0].shape, dtype=np.float64)
+    # TODO: revisit as this will likely hit memory limit in NCI
     phase_data = [i.phase_data for i in ifgs]
     for ifg in ifgs:
         ifg_phase_data_sum += ifg.phase_data
