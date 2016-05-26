8880c6877a6468f5b6bfd6eeb80841f661dd542e
Sudipta Basak <basaks@gmail.com>
2016-05-26 15:26:24 +1000

    [aps] now using pyrate speficic PyAPSGeo class

--- pyrate/remove_aps_delay.py
+++ pyrate/remove_aps_delay.py
@@ -153,14 +153,14 @@ def geo_correction(date_pair, mlooked_dem, dem_header, dem,
     """ using geo coordinates to remove APS """
 
     # TODO: remove mlooked_dem dependece on PyAPS_Geo class
-    aps1 = pa.PyAPS_geo(
+    aps1 = pa.PyAPSPyRateGeo(
         os.path.join(ECMWF_DIR, ECMWF_PRE + date_pair[0] + ECMWF_EXT),
-        mlooked_dem, grib=ECMWF, verb=True,
-        demfmt=GEOTIFF, demtype=np.float32, dem_header=dem_header)
-    aps2 = pa.PyAPS_geo(
+        grib=ECMWF, verb=True,
+        dem_header=dem_header)
+    aps2 = pa.PyAPSPyRateGeo(
         os.path.join(ECMWF_DIR, ECMWF_PRE + date_pair[1] + ECMWF_EXT),
-        mlooked_dem, grib=ECMWF, verb=True,
-        demfmt=GEOTIFF, demtype=np.float32, dem_header=dem_header)
+        grib=ECMWF, verb=True,
+        dem_header=dem_header)
     phs1 = np.zeros((aps1.ny, aps1.nx))
     phs2 = np.zeros((aps2.ny, aps2.nx))
     print 'Without Lat Lon files'
