eefd41f837bd852c74e089909949471ae101a006
sudipta <basaks@gmail.com>
2017-01-24 15:27:12 +1100

    pylint vcm

--- pyrate/vcm.py
+++ pyrate/vcm.py
@@ -1,17 +1,16 @@
-from __future__ import print_function
 """
 Variance/Covariance matrix functionality for PyRate, ported from Hua Wang's
 MATLAB code for Pirate.
 """
-
+from __future__ import print_function
 from numpy import array, where, isnan, real, imag, sum, sqrt, meshgrid
 from numpy import zeros, vstack, ceil, mean, exp, reshape
 from numpy.linalg import norm
 import numpy as np
 from scipy.fftpack import fft2, ifft2, fftshift
 from scipy.optimize import fmin
-from pyrate import shared
 
+from pyrate import shared
 from pyrate.algorithm import master_slave_ids
 
 
@@ -41,12 +40,15 @@ def unique_points(points):
 
 def cvd(ifg_path, params, calc_alpha=False):
     """
-    Calculate average covariance versus distance (autocorrelation) and its best
-    fitting exponential function
+    Calculate average covariance versus distance (autocorrelation) and its
+    best fitting exponential function
 
-    :param ifg: An interferogram.
-    :type ifg: :py:class:`pyrate.shared.Ifg`.
-    :param calc_alpha: whether you calculate alpha.
+    :param ifg_path: An interferogram.
+        ifg: :py:class:`pyrate.shared.Ifg`.
+    :param: params: dict
+        dict of config params
+    :param calc_alpha: bool
+        whether you calculate alpha.
     """
     if isinstance(ifg_path, str):  # used during MPI
         ifg = shared.Ifg(ifg_path)
@@ -76,82 +78,85 @@ def cvd(ifg_path, params, calc_alpha=False):
     # pixel distances from pixel at zero lag (image centre).
     xx, yy = meshgrid(range(ncols), range(nrows))
 
-    # r is distance from the center
+    # r_dist is distance from the center
     # doing np.divide and np.sqrt will improve performance as it keeps
     # calculations in the numpy land
-    r = np.divide(np.sqrt(((xx-ifg.x_centre) * ifg.x_size)**2 +
-             ((yy-ifg.y_centre) * ifg.y_size)**2), distfact)
+    r_dist = np.divide(np.sqrt(((xx-ifg.x_centre) * ifg.x_size)**2 +
+                               ((yy-ifg.y_centre) * ifg.y_size)**2), distfact)
 
-    r = reshape(r, ifg.num_cells)
+    r_dist = reshape(r_dist, ifg.num_cells)
     acg = reshape(autocorr_grid, ifg.num_cells)
 
     # Symmetry in image; keep only unique points
-    # tmp = unique_points(zip(acg, r))
+    # tmp = unique_points(zip(acg, r_dist))
     # Sudipta: Is this faster than keeping only the 1st half as in Matlab?
     # Sudipta: Unlikely, as unique_point is a search/comparison,
     # whereas keeping 1st half is just numpy indexing.
     # If it is not faster, why was this done differently here?
 
-    r = r[:int(ceil(ifg.num_cells/2.0)) + ifg.nrows]
-    acg = acg[:len(r)]
+    r_dist = r_dist[:int(ceil(ifg.num_cells/2.0)) + ifg.nrows]
+    acg = acg[:len(r_dist)]
 
     # Alternative method to remove duplicate cells from Matlab Pirate
-    #r = r[:ceil(len(r)/2)+nlines] # Reason for '+nlines' term unknown
+    # r_dist = r_dist[:ceil(len(r_dist)/2)+nlines]
+    #  Reason for '+nlines' term unknown
 
     # eg. array([x for x in set([(1,1), (2,2), (1,1)])])
-    # the above shortens r by some number of cells
+    # the above shortens r_dist by some number of cells
 
     # bin width for collecting data
-    w = max(ifg.x_size, ifg.y_size) * 2 / distfact
+    bin_width = max(ifg.x_size, ifg.y_size) * 2 / distfact
 
     # pick the smallest axis to determine circle search radius
-    #print 'ifg.X_CENTRE, ifg.Y_CENTRE=', ifg.x_centre, ifg.y_centre
-    #print 'ifg.X_SIZE, ifg.Y_SIZE', ifg.x_size, ifg.y_size
+    # print 'ifg.X_CENTRE, ifg.Y_CENTRE=', ifg.x_centre, ifg.y_centre
+    # print 'ifg.X_SIZE, ifg.Y_SIZE', ifg.x_size, ifg.y_size
     if (ifg.x_centre * ifg.x_size) < (ifg.y_centre * ifg.y_size):
         maxdist = ifg.x_centre * ifg.x_size / distfact
     else:
         maxdist = ifg.y_centre * ifg.y_size/ distfact
 
     # filter out data where the of lag distance is greater than maxdist
-    #r = array([e for e in rorig if e <= maxdist]) # MG: prefers to use all the data
-    #acg = array([e for e in rorig if e <= maxdist])
-    indices_to_keep = r < maxdist
-    r = r[indices_to_keep]
+    # r_dist = array([e for e in rorig if e <= maxdist]) #
+    # MG: prefers to use all the data
+    # acg = array([e for e in rorig if e <= maxdist])
+    indices_to_keep = r_dist < maxdist
+    r_dist = r_dist[indices_to_keep]
     acg = acg[indices_to_keep]
 
     if isinstance(ifg_path, str):
         ifg.close()
 
     if calc_alpha:
-        # classify values of r according to bin number
-        rbin = ceil(r / w).astype(int)
+        # classify values of r_dist according to bin number
+        rbin = ceil(r_dist / bin_width).astype(int)
         maxbin = max(rbin)  # consistent with Matlab code
 
         cvdav = zeros(shape=(2, maxbin))
 
         # the following stays in numpy land
         # distance instead of bin number
-        cvdav[0, :] = np.multiply(range(maxbin), w)
+        cvdav[0, :] = np.multiply(range(maxbin), bin_width)
         # mean variance for the bins
-        cvdav[1, :] = list(map(lambda b: mean(acg[rbin == b]), range(maxbin)))
+        cvdav[1, :] = [mean(acg[rbin == b]) for b in range(maxbin)]
 
-        # calculate best fit function maxvar*exp(-alpha*r)
-        alphaguess = 2 / (maxbin * w)
+        # calculate best fit function maxvar*exp(-alpha*r_dist)
+        alphaguess = 2 / (maxbin * bin_width)
         alpha = fmin(pendiffexp, x0=alphaguess, args=(cvdav,), disp=0,
                      xtol=1e-6, ftol=1e-6)
         print("1st guess alpha", alphaguess, 'converged alpha:', alpha)
-        # maximum variance usually at the zero lag: max(acg[:len(r)])
+        # maximum variance usually at the zero lag: max(acg[:len(r_dist)])
         return np.max(acg), alpha[0]
     else:
         return np.max(acg), None
 
 
 def get_vcmt(ifgs, maxvar):
-    '''
+    """
     Returns the temporal variance/covariance matrix.
-    '''
+    """
+    # c=0.5 for common master or slave; c=-0.5 if master
+    # of one matches slave of another
 
-    # c=0.5 for common master or slave; c=-0.5 if master of one matches slave of another
     nifgs = len(ifgs)
     vcm_pat = zeros((nifgs, nifgs))
 
