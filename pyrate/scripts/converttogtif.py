36921a873f3fe1865ee6e71e20f704ad06e64037
sudipta <basaks@gmail.com>
2017-02-23 10:34:17 +1100

    docstrings and pylint

--- pyrate/scripts/converttogtif.py
+++ pyrate/scripts/converttogtif.py
@@ -1,8 +1,7 @@
-from __future__ import print_function
 """
 Script to convert Gamma headers to ESRI's BIL format.
 """
-
+from __future__ import print_function
 import sys
 import luigi
 from pyrate.tasks.utils import pythonify_config
@@ -10,13 +9,17 @@ from pyrate.tasks import ConvertToGeotiff
 
 
 def main():
+    """
+    Wrapper function to convert unwrapped interferrograms into geotifs
+    for all data types
+    """
     usage = 'Usage: gamma.py <config file>'
     if len(sys.argv) == 1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print(usage)
         return
-    rawConfigFile = sys.argv[1]     # this does '~' expansion automatically
+    raw_config_file = sys.argv[1]     # this does '~' expansion automatically
     luigi.configuration.LuigiConfigParser.add_config_path(
-        pythonify_config(rawConfigFile))
+        pythonify_config(raw_config_file))
     luigi.build([ConvertToGeotiff()], local_scheduler=True)
 
 
--- pyrate/scripts/main.py
+++ pyrate/scripts/main.py
@@ -1,6 +1,8 @@
-import click
-import logging
+"""Pyrate execution main module"""
+
 from os.path import abspath
+import logging
+import click
 from pyrate import pyratelog as pylog
 from pyrate import config as cf
 from pyrate.scripts import run_prepifg, run_pyrate, postprocessing
@@ -13,12 +15,17 @@ log = logging.getLogger(__name__)
               type=click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR']),
               default='INFO', help='Level of logging')
 def cli(verbosity):
+    """Commandline options and logging setup"""
     pylog.configure(verbosity)
 
 
 @cli.command()
 @click.argument('config_file')
 def prepifg(config_file):
+    """
+    Prepifg wrapper. This performs both geotif coverserion and
+    also multilooking (resampling) and cropping
+    """
     config_file = abspath(config_file)
     params = cf.get_config_params(config_file)
     run_prepifg.main(params)
@@ -31,6 +38,7 @@ def prepifg(config_file):
 @click.option('-c', '--cols', type=int, default=1,
               help='divide ifgs into this many columns')
 def linrate(config_file, rows, cols):
+    """ Time series and linear rate computation"""
     config_file = abspath(config_file)
     run_pyrate.main(config_file, rows, cols)
 
@@ -38,9 +46,12 @@ def linrate(config_file, rows, cols):
 @cli.command()
 @click.argument('config_file')
 @click.option('-r', '--rows', type=int, default=1,
-              help='divide ifgs into this many rows')
+              help='divide ifgs into this many rows. Must be same as '
+                   'number of rows used in linrate')
 @click.option('-c', '--cols', type=int, default=1,
-              help='divide ifgs into this many columns')
+              help='divide ifgs into this many columns. Must be same as '
+                   'number of cols used in linrate')
 def postprocess(config_file, rows, cols):
+    """ assemble the linrate and timeseries tiles back together"""
     config_file = abspath(config_file)
     postprocessing.main(config_file, rows, cols)
