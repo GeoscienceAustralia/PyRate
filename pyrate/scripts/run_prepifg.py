8a0197ab815435bca84fff1d8e2cd48b0cc629a4
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2017-03-02 17:15:44 +1100

    add boilerplate and descriptions to scripts

--- pyrate/scripts/run_prepifg.py
+++ pyrate/scripts/run_prepifg.py
@@ -1,8 +1,24 @@
+"""
+This Python script is part of the PyRate software package
+
+This Python script converts ROI_PAC or GAMMA format unwrapped 
+interferograms into geotiffs and applies optional multilooking and cropping.
+
+Copyright 2017 Geoscience Australia
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 # -*- coding: utf-8 -*-
-"""
-This script converts roipac or gamma format unwrapped interferrograms into
- geotifs first and then optionally multilook and crop.
-"""
 from __future__ import print_function
 import sys
 import os
