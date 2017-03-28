21636491baca5c43a88369a8255712e2e3c52023
Sudipta Basak <basaks@gmail.com>
2017-03-28 15:27:07 +1100

    [aps, slpfilter] tests for butterworth and gaussian filters

--- tests/test_aps.py
+++ tests/test_aps.py
@@ -5,9 +5,10 @@ import pytest
 
 from pyrate.algorithm import get_epochs
 from pyrate.aps.temporal import tlpfilter
+from pyrate.aps.spatial import _slp_filter
 from pyrate import config as cf
 from pyrate.compat import pickle, PY3
-from tests.common import SML_TEST_DIR, TEST_CONF_GAMMA
+from tests.common import SML_TEST_DIR, TEST_CONF_GAMMA, small_data_setup
 
 # tsincr matrix from matlab using svd timeseries method
 tsincr_svd = sio.loadmat(os.path.join(SML_TEST_DIR, 'matlab_aps',
@@ -23,6 +24,8 @@ ts_hp_m2 = sio.loadmat(os.path.join(SML_TEST_DIR, 'matlab_aps',
                                     'ts_hp_m2.mat'))
 ts_hp_m3 = sio.loadmat(os.path.join(SML_TEST_DIR, 'matlab_aps',
                                     'ts_hp_m3.mat'))
+epochlist = get_epochs(ifgs_pk)[0]
+params = cf.get_config_params(os.path.join(TEST_CONF_GAMMA))
 
 
 @pytest.fixture(params=[1, 2, 3])
@@ -30,9 +33,12 @@ def tlpfilter_method(request):
     return request.param
 
 
+@pytest.fixture(params=[1, 2])
+def slpfilter_method(request):
+    return request.param
+
+
 def test_tlpfilter_matlab(tlpfilter_method):
-    epochlist = get_epochs(ifgs_pk)[0]
-    params = cf.get_config_params(os.path.join(TEST_CONF_GAMMA))
     params[cf.TLPF_METHOD] = tlpfilter_method
     ts = ts_hp if tlpfilter_method == 1  \
         else ts_hp_m2 if tlpfilter_method == 2 \
@@ -42,3 +48,29 @@ def test_tlpfilter_matlab(tlpfilter_method):
     tsfilt_incr_matlab = ts['ts_hp']
     np.testing.assert_almost_equal(tsfilt_incr_matlab,
                                    tsfilt_incr, decimal=4)
+
+tsincr = tsincr_svd['tsincr']
+
+params[cf.TLPF_METHOD] = 3
+ts_hp_before_slpfilter = tsincr - tlpfilter(tsincr, epochlist, params)
+ts_aps_m1 = sio.loadmat(os.path.join(SML_TEST_DIR, 'matlab_aps',
+                                     'ts_aps.mat'))['ts_aps']
+ts_aps_m2 = sio.loadmat(os.path.join(SML_TEST_DIR, 'matlab_aps',
+                                     'ts_aps_m2.mat'))['ts_aps']
+
+
+def test_slpfilter_matlab(slpfilter_method):
+    params[cf.TLPF_METHOD] = slpfilter_method
+    ifgs = small_data_setup()
+    xpsize = 76.834133036409
+    ypsize = 92.426722191659
+    ts_aps = np.zeros_like(ts_aps_m1)
+    rows, cols = ifgs[0].shape
+    for i in range(ts_aps.shape[2]):
+        ts_aps[:, :, i] = _slp_filter(cutoff=params[cf.SLPF_CUTOFF],
+                                      rows=rows, cols=cols,
+                                      x_size=xpsize, y_size=ypsize,
+                                      params=params,
+                                      phase=ts_hp_before_slpfilter[:, :, i])
+
+    np.testing.assert_array_almost_equal(ts_aps, ts_aps_m1, decimal=4)
