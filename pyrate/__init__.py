d0a634bff0ab79420b9e28abd21f1f48146f642c
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2017-05-22 15:42:23 +1000

    update version to 0.2.0

--- pyrate/__init__.py
+++ pyrate/__init__.py
@@ -7,4 +7,4 @@ PyRate
 Main package for PyRate.
 """
 
-__version__ = '0.1.3'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from subprocess import check_output
 import sys
 
 python_version = sys.version_info
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 # numpy support for python3.3 not available for version > 1.10.1
 if python_version.major == 3 and python_version.minor == 3:
