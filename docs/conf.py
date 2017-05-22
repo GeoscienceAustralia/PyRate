a60728f960f8b3777066c6e83388d13ba5665e5c
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2017-05-22 12:21:10 +1000

    update conf.py to make Sphinx RTD theme work

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -94,7 +94,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
