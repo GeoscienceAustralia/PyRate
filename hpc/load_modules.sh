69363ad4e1e2e74afd77cf4a1968b62b48939dad
Bren Moushall <bmoush@gmail.com>
2019-07-18 10:56:46 +1000

    Add localisation env var to raijin setup script.

--- hpc/load_modules.sh
+++ hpc/load_modules.sh
@@ -7,3 +7,6 @@ module load openmpi/2.1.1
 
 # Remove the prebuilt GDAL Python bindings that get added by the NCI module.
 PYTHONPATH=`echo $PYTHONPATH | sed -e 's|/apps/gdal/.*:||'`
+# Required by Click
+export LC_ALL=en_AU.UTF-8
+export LANG=en_AU.UTF-8
