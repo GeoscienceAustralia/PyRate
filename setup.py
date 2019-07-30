909717e74e461e44d991e3616dec9ee5a4d3939c
Bren Moushall <bmoush@gmail.com>
2019-07-30 23:11:45 +0000

    Add Python 3.7 as supported version.

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
