8ff9b5351902968e2db45d5b2b117c230bb04f72
sheecegardezi <sheecegardezi@gmail.com>
2019-10-16 16:18:33 +1100

    update boilerplate for coherence masking unit tests

--- tests/test_coherence.py
+++ tests/test_coherence.py
@@ -6,36 +6,24 @@ import copy
 
 import pyrate.core.config as cf
 from pyrate import converttogeotif, prepifg
-from tests import common
+from pyrate.core import gdal_python
+
 
 class CoherenceMaskingTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.params = cf.get_config_params(common.TEST_CONF_GAMMA)
-        cls.params[cf.COH_MASK] = 1
-        cls.params[cf.COH_FILE_DIR] = common.SML_TEST_COH_DIR
-        cls.params[cf.COH_FILE_LIST] = common.SML_TEST_COH_LIST
 
-    def teardown_method(self, method):
-        common.remove_tifs(self.params[cf.OBS_DIR])
-        common.remove_tifs(self.params[cf.COH_FILE_DIR])
-
-    def test_find_coherence_files_for_ifgs(self):
-        ifg_paths = [os.path.join(self.params[cf.OBS_DIR], ifg) 
-                     for ifg in cf.parse_namelist(self.params[cf.IFG_FILE_LIST])]
-        coherence_files = [cf.coherence_paths_for(path, self.params, tif=False)
-                           for path in ifg_paths]
-        self.assertEqual(len(coherence_files), 17)
 
     def test_coherence_files_not_converted(self):
-        self.params[cf.COH_MASK] = 0
-        gtiff_paths = converttogeotif.main(self.params)
-        coh_files = [path for path in gtiff_paths if '_cc.tif' in path]
-        self.assertEqual(len(coh_files), 0)
-        self.params[cf.COH_MASK] = 1
-        
-    # TOD: Testing actual coherence masking
-    # Need coherence file test data
-    # Need pre-masked IFGs
-    # Compare that output of coherence masking matches pre-masked IFGs   
 
+        # create a gdal data set
+        # create a coherence mask dataset
+        # create a artificial resultant data set
+
+        # use the gdal_python.coherence_masking to find the resultant mask data set
+
+        # compare the artificial and actual resultant data set
+        self.assertTrue(True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
