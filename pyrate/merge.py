167958bcdb0ba648941d4bfd72f0cb86013cb79b
sheecegardezi <sheecegardezi@gmail.com>
2019-10-22 14:08:05 +1100

    create quick view png

--- pyrate/merge.py
+++ pyrate/merge.py
@@ -23,6 +23,7 @@ import logging
 import pickle as cp
 import numpy as np
 from osgeo import gdal
+import subprocess
 
 from pyrate.core import shared, ifgconstants as ifc, mpiops, config as cf
 from pyrate.core.shared import PrereadIfg
@@ -42,6 +43,39 @@ def main(params, rows, cols):
     _merge_linrate(rows, cols, params)
     if params[cf.TIME_SERIES_CAL]:
         _merge_timeseries(rows, cols, params)
+
+    log.info('Start creating quicklook results.')
+    output_folder_path = os.path.dirname(params["tmpdir"])
+
+    # open raster and choose band to find min, max
+    raster = os.path.join(output_folder_path, "linrate.tif")
+    gtif = gdal.Open(raster)
+    srcband = gtif.GetRasterBand(1)
+
+    # Get raster statistics
+    minimum, maximum, mean, stddev = srcband.GetStatistics(True, True)
+    maximum = max(abs(minimum), abs(maximum))
+    minimum = -1 * maximum
+    step = (maximum - minimum)/256.0
+
+    # read color map from utilites and write it to the output folder
+    with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "utils", "colormap.txt"),"r") as f:
+        color_map_list = []
+        for line in f.readlines():
+            color_map_list.append(line.strip().split(" "))
+
+    for i, no in enumerate(np.arange(minimum, maximum, step)):
+        color_map_list[i][0] = str(no)
+
+    color_map_path = os.path.join(output_folder_path,  "colormap.txt")
+    with open(color_map_path, "w") as f:
+        for line in color_map_list:
+            f.write(' '.join(line) + "\n")
+
+    input_tif_path = os.path.join(output_folder_path, "linrate.tif")
+    output_png_path = os.path.join(output_folder_path, "linrate.png")
+    subprocess.check_call(["gdaldem", "color-relief", input_tif_path, color_map_path, output_png_path, "-nearest_color_entry"])
+    log.info('Finished creating quicklook results.')
 
 
 def _merge_linrate(rows, cols, params):
@@ -98,6 +132,7 @@ def _save_linrate(ifgs_dict, params, tiles, out_type):
     shared.write_output_geotiff(md, gt, wkt, rate, dest, np.nan)
     npy_rate_file = os.path.join(params[cf.OUT_DIR], out_type + '.npy')
     np.save(file=npy_rate_file, arr=rate)
+
     log.info('Finished PyRate merging {}'.format(out_type))
 
 
