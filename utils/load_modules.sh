bf2caff8d952742f4cba575ccd860e0b6c38c48b
sheecegardezi <sheecegardezi@gmail.com>
2019-12-06 12:35:44 +1100

    use latest git module on NCI

--- utils/load_modules.sh
+++ utils/load_modules.sh
@@ -4,9 +4,10 @@ module unload openmpi
 module load python3/3.7.2
 module load gdal/2.2.2
 module load openmpi/2.1.1
-
 # Remove the prebuilt GDAL Python bindings that get added by the NCI module.
 PYTHONPATH=`echo $PYTHONPATH | sed -e 's|/apps/gdal/.*:||'`
 # Required by Click
 export LC_ALL=en_AU.UTF-8
 export LANG=en_AU.UTF-8
+module unload git
+module load git
