67e01e40294c23538ff52d092f53bba955749860
sheecegardezi <sheecegardezi@gmail.com>
2019-12-09 20:55:31 +1100

    updated packages

--- utils/load_modules.sh
+++ utils/load_modules.sh
@@ -1,9 +1,9 @@
 module unload intel-cc
 module unload intel-fc
 module unload openmpi
-module load python3/3.7.2
-module load gdal/2.2.2
-module load openmpi/2.1.1
+module load python3/3.7.4
+module load gdal/3.0.2
+module load openmpi/2.1.6
 # Remove the prebuilt GDAL Python bindings that get added by the NCI module.
 PYTHONPATH=`echo $PYTHONPATH | sed -e 's|/apps/gdal/.*:||'`
 # Required by Click
