3744e99f7175c2d9954b48c373e55b6b9c3c04ee
sheecegardezi <sheecegardezi@gmail.com>
2020-01-16 12:17:25 +1100

    Feature: Update running test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+module purge
+module load python3/3.7.4
+module load gdal/3.0.2
+module load openmpi/2.1.6
+export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPAT
 source ~/PyRateVenv/bin/activate
 pip install -U pytest
 cd ~/PyRate
-pytest tests/
+pytest tests/test_algorithm
