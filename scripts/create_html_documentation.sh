7e99307d0c7d4292af0479493051387741c4a189
sheecegardezi <sheecegardezi@gmail.com>
2020-01-17 14:06:02 +1100

    BugFix: set env before exe script

--- scripts/create_html_documentation.sh
+++ scripts/create_html_documentation.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+module purge
+module load python3/3.7.4
+module load gdal/3.0.2
+module load openmpi/2.1.6
+export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
+source ~/PyRateVenv/bin/activate
 cd ~/PyRate/docs
 make html
 echo "Use following cmd to copy file to local machine:"
