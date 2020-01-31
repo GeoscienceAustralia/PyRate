6f8c755b93a8c2ddc48890f3a73a4f4964c179ea
sheecegardezi <sheecegardezi@gmail.com>
2020-01-31 16:34:19 +1100

    Feature: migrate to new config module

--- pyrate/conv2tif.py
+++ pyrate/conv2tif.py
@@ -46,23 +46,15 @@ def main(params=None):
     # Going to assume base_ifg_paths is ordered correcly
     # pylint: disable=too-many-branches
 
-    usage = 'Usage: pyrate conv2tif -f <config_file>'
-    if mpiops.size > 1:  # Over-ride input options if this is an MPI job
-        params[cf.PARALLEL] = False
-
-    if params:
-        base_ifg_paths = cf.original_ifg_paths(params[cf.IFG_FILE_LIST], params[cf.OBS_DIR])
-    else:  # if params not provided read from config file
-        if (not params) and (len(sys.argv) < 3):
-            print(usage)
-            return
-        base_ifg_paths, _, params = cf.get_ifg_paths(sys.argv[2])
-        raw_config_file = sys.argv[2]
+    base_ifg_paths = []
+    for interferogram_file in params["interferogram_files"]:
+        base_ifg_paths.append(interferogram_file.unwrapped_path)
 
     if params[cf.COH_MASK]:
         base_ifg_paths.extend(cf.coherence_paths(params))
 
-    if params[cf.DEM_FILE] is not None: # optional DEM conversion
+    # optional DEM conversion
+    if params[cf.DEM_FILE] is not None:
         base_ifg_paths.append(params[cf.DEM_FILE])
 
     processor = params[cf.PROCESSOR]  # roipac or gamma
@@ -72,35 +64,15 @@ def main(params=None):
         if params[cf.APS_ELEVATION_MAP]:
             base_ifg_paths.append(params[cf.APS_ELEVATION_MAP])
 
-    process_base_ifgs_paths = np.array_split(base_ifg_paths, mpiops.size)[mpiops.rank]
-    gtiff_paths = do_geotiff(process_base_ifgs_paths, params)
-    log.info("Finished conv2tif")
-    return gtiff_paths
+    process_ifgs_paths = np.array_split(base_ifg_paths, mpiops.size)[mpiops.rank]
 
-
-def do_geotiff(base_unw_paths, params):
-    """
-    Convert input interferograms to geotiff format.
-
-    :param list base_unw_paths: List of unwrapped interferograms
-    :param dict params: Parameters dictionary corresponding to config file
-    """
-    # pylint: disable=expression-not-assigned
     log.info("Converting input interferograms to geotiff")
-    parallel = params[cf.PARALLEL]
 
-    if parallel:
-        log.info("Running geotiff conversion in parallel with {} "
-                 "processes".format(params[cf.PROCESSES]))
-        dest_base_ifgs = Parallel(n_jobs=params[cf.PROCESSES], 
-                         verbose=shared.joblib_log_level(cf.LOG_LEVEL))(
-            delayed(_geotiff_multiprocessing)(p, params)
-            for p in base_unw_paths)
-    else:
-        log.info("Running geotiff conversion in serial")
-        dest_base_ifgs = [_geotiff_multiprocessing(b, params)
-                          for b in base_unw_paths]
-    return dest_base_ifgs
+    log.info("Running geotiff conversion in serial")
+    for b in process_ifgs_paths:
+        _geotiff_multiprocessing(b, params)
+
+    log.info("Finished conv2tif")
 
 
 def _geotiff_multiprocessing(unw_path, params):
@@ -124,8 +96,7 @@ def _geotiff_multiprocessing(unw_path, params):
             header = roipac.roipac_header(unw_path, params)
         else:
             raise PreprocessError('Processor must be ROI_PAC (0) or GAMMA (1)')
-        shared.write_fullres_geotiff(header, unw_path, dest,
-                                     nodata=params[cf.NO_DATA_VALUE])
+        shared.write_fullres_geotiff(header, unw_path, dest, nodata=params[cf.NO_DATA_VALUE])
         return dest
     else:
         log.info("Full-res geotiff already exists")
