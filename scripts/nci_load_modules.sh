80ad16a1175370f1b2a7c8f581563c19b3ce3491
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2020-02-07 13:58:30 +1100

    Bugfix: update NCI scripts

--- scripts/nci_load_modules.sh
+++ scripts/nci_load_modules.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-rm -rf  ~/PyRateVenv
-rm -rf  ~/PyRate
-git clone https://github.com/GeoscienceAustralia/PyRate.git --branch test --single-branch ~/PyRate
 module purge
 module load python3/3.7.4
 module load gdal/3.0.2
 module load openmpi/2.1.6
 export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
-python3 -m venv ~/PyRateVenv
 source ~/PyRateVenv/bin/activate
-cd ~/PyRate
-python setup.py install
