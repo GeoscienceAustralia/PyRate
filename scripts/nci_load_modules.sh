5e52d34a247e1645a4ba04838068b2856b6fd6a6
Geoffrey Squire <ggs599@gadi-login-02.gadi.nci.org.au>
2020-02-26 11:54:19 +1100

    Configurable venv location in nci_load_modules.

--- scripts/nci_load_modules.sh
+++ scripts/nci_load_modules.sh
@@ -4,4 +4,4 @@ module load python3/3.7.4
 module load gdal/3.0.2
 module load openmpi/2.1.6
 export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
-source ~/PyRateVenv/bin/activate
+source ${PYRATE_VENV:-${HOME}/PyRateVenv}/bin/activate
