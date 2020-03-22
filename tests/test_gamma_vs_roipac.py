5015fb3c98010cd507a50906cc611e3ce0315b38
sheecegardezi <sheecegardezi@gmail.com>
2020-03-23 07:32:54 +1100

    BugFix: fix config files generated on fly

--- tests/test_gamma_vs_roipac.py
+++ tests/test_gamma_vs_roipac.py
@@ -87,38 +87,34 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
         """
         with open(self.conf_file, "w") as conf:
             conf.write("[{}]\n".format(DUMMY_SECTION_NAME))
-            conf.write("{}: {}\n".format(NO_DATA_VALUE, "0.0"))
+            conf.write("{}: {}\n".format("noDataValue", "0.0"))
             conf.write("{}: {}\n".format(OBS_DIR, self.SMLNEY_GAMMA_TEST))
-            conf.write("{}: {}\n".format(OUT_DIR, self.base_dir))
+            conf.write("{}: {}\n".format("outdir", self.base_dir))
             conf.write("{}: {}\n".format(IFG_FILE_LIST, self.ifgListFile))
             conf.write("{}: {}\n".format(PROCESSOR, "1"))
             conf.write("{}: {}\n".format(DEM_HEADER_FILE, os.path.join(self.SMLNEY_GAMMA_TEST, "20060619_utm_dem.par")))
             conf.write("{}: {}\n".format(IFG_LKSX, "1"))
             conf.write("{}: {}\n".format(IFG_LKSY, "1"))
             conf.write("{}: {}\n".format(IFG_CROP_OPT, "1"))
-            conf.write("{}: {}\n".format(NO_DATA_AVERAGING_THRESHOLD, "0.5"))
+            conf.write("{}: {}\n".format("noDataAveragingThreshold", "0.5"))
             conf.write("{}: {}\n".format(SLC_DIR, ""))
             conf.write("{}: {}\n".format(SLC_FILE_LIST, common.SML_TEST_GAMMA_HEADER_LIST))
             conf.write("{}: {}\n".format(DEM_FILE, common.SML_TEST_DEM_GAMMA))
             conf.write("{}: {}\n".format(APS_INCIDENCE_MAP, common.SML_TEST_INCIDENCE))
             conf.write("{}: {}\n".format(APS_ELEVATION_MAP, ""))
             conf.write("{}: {}\n".format("refny", ""))
-            conf.write("{}: {}\n".format("noDataValue", ""))
             conf.write("{}: {}\n".format("tlpfcutoff", ""))
             conf.write("{}: {}\n".format("axsig", ""))
             conf.write("{}: {}\n".format("slpfmethod", ""))
             conf.write("{}: {}\n".format("orbfit", ""))
             conf.write("{}: {}\n".format("slcfilelist", ""))
-            conf.write("{}: {}\n".format("obsdir", ""))
             conf.write("{}: {}\n".format("processes", ""))
             conf.write("{}: {}\n".format("orbfitlksy", ""))
             conf.write("{}: {}\n".format("nan_conversion", ""))
             conf.write("{}: {}\n".format("refchipsize", ""))
             conf.write("{}: {}\n".format("slpfcutoff", ""))
             conf.write("{}: {}\n".format("cohfilelist", ""))
-            conf.write("{}: {}\n".format("demHeaderFile", ""))
             conf.write("{}: {}\n".format("cohfiledir", ""))
-            conf.write("{}: {}\n".format("processor", ""))
             conf.write("{}: {}\n".format("fgxfirst", ""))
             conf.write("{}: {}\n".format("refnx", ""))
             conf.write("{}: {}\n".format("slcFileDir", ""))
@@ -128,21 +124,16 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
             conf.write("{}: {}\n".format("cohmask", ""))
             conf.write("{}: {}\n".format("ifgylast", ""))
             conf.write("{}: {}\n".format("smorder", ""))
-            conf.write("{}: {}\n".format("ifglksx", ""))
             conf.write("{}: {}\n".format("ifgxlast", ""))
             conf.write("{}: {}\n".format("apsest", ""))
             conf.write("{}: {}\n".format("tlpfmethod", ""))
             conf.write("{}: {}\n".format("cohthresh", ""))
             conf.write("{}: {}\n".format("tscal", ""))
             conf.write("{}: {}\n".format("pthr", ""))
-            conf.write("{}: {}\n".format("noDataAveragingThreshold", ""))
             conf.write("{}: {}\n".format("tsmethod", ""))
             conf.write("{}: {}\n".format("smfactor", ""))
             conf.write("{}: {}\n".format("parallel", ""))
             conf.write("{}: {}\n".format("orbfitdegrees", ""))
-            conf.write("{}: {}\n".format("ifgfilelist", ""))
-            conf.write("{}: {}\n".format("ifgcropopt", ""))
-            conf.write("{}: {}\n".format("outdir", ""))
             conf.write("{}: {}\n".format("refx", ""))
             conf.write("{}: {}\n".format("orbfitmethod", ""))
             conf.write("{}: {}\n".format("refy", ""))
@@ -151,9 +142,7 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
             conf.write("{}: {}\n".format("ifgyfirst", ""))
             conf.write("{}: {}\n".format("refminfrac", ""))
             conf.write("{}: {}\n".format("refest", ""))
-            conf.write("{}: {}\n".format("demfile", ""))
             conf.write("{}: {}\n".format("slpnanfill", ""))
-            conf.write("{}: {}\n".format("ifglksy", ""))
             conf.write("{}: {}\n".format("slpforder", ""))
 
         with open(self.ifgListFile, "w") as ifgl:
@@ -163,34 +152,6 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
         self.conf_file = self.gamma_conffile
         self.base_dir = self.gamma_base_dir
         self.ifgListFile = self.gamma_ifgListFile
-        self.check_gamma(self.gamma_conffile)
-
-    def check_gamma(self, conf_file):
-        """
-
-        Args:
-          conf_file: 
-
-        Returns:
-
-        """
-        data_paths = glob.glob(os.path.join(self.SMLNEY_GAMMA_TEST, "*_utm.unw"))
-
-        self.make_gamma_input_files(data_paths)
-
-        base_ifg_paths, dest_paths, params = cf.get_ifg_paths(conf_file)
-        dest_base_ifgs = [
-            os.path.join(params[cf.OBS_DIR],
-                         os.path.basename(q).split(".")[0] + "_" + os.path.basename(q).split(".")[1] + ".tif")
-            for q in base_ifg_paths
-        ]
-
-        conv2tif_handler(conf_file)
-        prepifg_handler(conf_file)
-
-        for p, q in zip(dest_base_ifgs, dest_paths):
-            self.assertTrue(os.path.exists(p), "{} does not exist".format(p))
-            self.assertTrue(os.path.exists(q), "{} does not exist".format(q))
 
     def make_roipac_input_files(self, data, projection):
         """
@@ -205,36 +166,29 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
         with open(self.confFile, "w") as conf:
             conf.write("[{}]\n".format(DUMMY_SECTION_NAME))
             conf.write("{}: {}\n".format(INPUT_IFG_PROJECTION, projection))
-            conf.write("{}: {}\n".format(NO_DATA_VALUE, "0.0"))
+            conf.write("{}: {}\n".format("noDataValue", "0.0"))
             conf.write("{}: {}\n".format(OBS_DIR, common.SML_TEST_OBS))
-            conf.write("{}: {}\n".format(OUT_DIR, self.base_dir))
+            conf.write("{}: {}\n".format("outdir", self.base_dir))
             conf.write("{}: {}\n".format(IFG_FILE_LIST, self.ifgListFile))
             conf.write("{}: {}\n".format(PROCESSOR, "0"))
             conf.write("{}: {}\n".format(DEM_HEADER_FILE, common.SML_TEST_DEM_HDR))
             conf.write("{}: {}\n".format(IFG_LKSX, "1"))
             conf.write("{}: {}\n".format(IFG_LKSY, "1"))
             conf.write("{}: {}\n".format(IFG_CROP_OPT, "1"))
-            conf.write("{}: {}\n".format(NO_DATA_AVERAGING_THRESHOLD, "0.5"))
+            conf.write("{}: {}\n".format("noDataAveragingThreshold", "0.5"))
             conf.write("{}: {}\n".format(DEM_FILE, common.SML_TEST_DEM_ROIPAC))
             conf.write("{}: {}\n".format(APS_INCIDENCE_MAP, ""))
             conf.write("{}: {}\n".format(APS_ELEVATION_MAP, ""))
-            conf.write("{}: {}\n".format("ifgfilelist", ""))
             conf.write("{}: {}\n".format("slpforder", ""))
-            conf.write("{}: {}\n".format("demHeaderFile", ""))
-            conf.write("{}: {}\n".format("ifglksx", ""))
             conf.write("{}: {}\n".format("cohthresh", ""))
             conf.write("{}: {}\n".format("tlpfmethod", ""))
             conf.write("{}: {}\n".format("slcFileDir", ""))
-            conf.write("{}: {}\n".format("ifgcropopt", ""))
-            conf.write("{}: {}\n".format("ifglksy", ""))
-            conf.write("{}: {}\n".format("demfile", ""))
             conf.write("{}: {}\n".format("cohfilelist", ""))
             conf.write("{}: {}\n".format("orbfit", ""))
             conf.write("{}: {}\n".format("ts_pthr", ""))
             conf.write("{}: {}\n".format("slpnanfill_method", ""))
             conf.write("{}: {}\n".format("tscal", ""))
             conf.write("{}: {}\n".format("refy", ""))
-            conf.write("{}: {}\n".format("processor", ""))
             conf.write("{}: {}\n".format("slpnanfill", ""))
             conf.write("{}: {}\n".format("orbfitdegrees", ""))
             conf.write("{}: {}\n".format("ifgxfirst", ""))
@@ -243,19 +197,15 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
             conf.write("{}: {}\n".format("refnx", ""))
             conf.write("{}: {}\n".format("tlpfcutoff", ""))
             conf.write("{}: {}\n".format("processes", ""))
-            conf.write("{}: {}\n".format("outdir", ""))
             conf.write("{}: {}\n".format("refny", ""))
             conf.write("{}: {}\n".format("nsig", ""))
             conf.write("{}: {}\n".format("tsmethod", ""))
             conf.write("{}: {}\n".format("ifgxlast", ""))
             conf.write("{}: {}\n".format("refchipsize", ""))
-            conf.write("{}: {}\n".format("noDataValue", ""))
-            conf.write("{}: {}\n".format("noDataAveragingThreshold", ""))
             conf.write("{}: {}\n".format("slcfilelist", ""))
             conf.write("{}: {}\n".format("ifgyfirst", ""))
             conf.write("{}: {}\n".format("cohmask", ""))
             conf.write("{}: {}\n".format("orbfitmethod", ""))
-            conf.write("{}: {}\n".format("obsdir", ""))
             conf.write("{}: {}\n".format("refminfrac", ""))
             conf.write("{}: {}\n".format("maxsig", ""))
             conf.write("{}: {}\n".format("smorder", ""))
@@ -283,16 +233,6 @@ class TestGammaVsRoipacEquality(unittest.TestCase):
         self.confFile = self.roipac_conffile
         self.ifgListFile = self.roipac_ifgListFile
         self.base_dir = self.roipac_base_dir
-        self.check_roipac()
-
-    def check_roipac(self):
-        """ """
-        self.make_roipac_input_files(self.dataPaths, "WGS84")
-
-        conv2tif_handler(self.confFile)
-        prepifg_handler(self.confFile)
-        for path in self.expPaths:
-            self.assertTrue(os.path.exists(path), "{} does not exist".format(path))
 
     def test_equality_of_meta_data(self):
         """ """
