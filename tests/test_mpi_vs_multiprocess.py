e3595de835c37a8129ae6dbd9af719eb6319e728
Sudipta Basak <basaks@gmail.com>
2020-04-13 20:29:16 +1000

    fixed the system mpi vs multiprocess tests

--- tests/test_mpi_vs_multiprocess.py
+++ tests/test_mpi_vs_multiprocess.py
@@ -30,11 +30,16 @@ def modified_config(tempdir, get_lks, get_crop, orbfit_lks, orbfit_method, orbfi
         params[cf.OUT_DIR] = tdir.joinpath('out').as_posix()
         params[cf.PARALLEL] = 1
         params[cf.APSEST] = 1
-        params[cf.IFG_LKSX] = get_lks
+        params[cf.IFG_LKSX], params[cf.IFG_LKSY] = get_lks, get_lks
         params[cf.DEM_FILE] = tdir.joinpath(Path(params[cf.DEM_FILE]).name).as_posix()
         params[cf.DEM_HEADER_FILE] = tdir.joinpath(Path(params[cf.DEM_HEADER_FILE]).name).as_posix()
         params[cf.SLC_FILE_LIST] = tdir.joinpath(Path(params[cf.SLC_FILE_LIST]).name).as_posix()
         params[cf.SLC_DIR] = tdir.as_posix()
+        params[cf.IFG_FILE_LIST] = tdir.joinpath(Path(params[cf.IFG_FILE_LIST]).name).as_posix()
+        params[cf.COH_FILE_DIR] = tdir.as_posix()
+        params[cf.APS_INCIDENCE_MAP] = tdir.joinpath(Path(params[cf.APS_INCIDENCE_MAP]).name).as_posix()
+        params[cf.REFNX], params[cf.REFNY] = 4, 4
+        params[cf.TMPDIR] = tdir.joinpath(Path(params[cf.TMPDIR]).name).as_posix()
 
         params[cf.IFG_CROP_OPT] = get_crop
         params[cf.ORBITAL_FIT_LOOKS_X], params[cf.ORBITAL_FIT_LOOKS_Y] = orbfit_lks, orbfit_lks
@@ -42,7 +47,8 @@ def modified_config(tempdir, get_lks, get_crop, orbfit_lks, orbfit_method, orbfi
         params[cf.ORBITAL_FIT_METHOD] = orbfit_method
         params[cf.ORBITAL_FIT_DEGREE] = orbfit_degrees
         params[cf.REF_EST_METHOD] = ref_est_method
-
+        params["rows"], params["cols"] = 3, 2
+        print(params)
         # write new temp config
         output_conf = tdir.joinpath(output_conf_file)
         cf.write_config_file(params=params, output_conf_file=output_conf)
@@ -52,6 +58,8 @@ def modified_config(tempdir, get_lks, get_crop, orbfit_lks, orbfit_method, orbfi
 
 
 def test_conv2tif_prepifg_parallel_vs_mpi(modified_config, roipac_or_gamma_conf):
+    print("\n\n")
+    print("===x==="*10)
     mpi_conf, params = modified_config(roipac_or_gamma_conf, 'mpi_conf.conf')
 
     check_call(f"mpirun -n 3 pyrate conv2tif -f {mpi_conf}", shell=True)
@@ -64,13 +72,15 @@ def test_conv2tif_prepifg_parallel_vs_mpi(modified_config, roipac_or_gamma_conf)
     check_call(f"pyrate prepifg -f {sr_conf}", shell=True)
     check_call(f"pyrate process -f {sr_conf}", shell=True)
 
+
     # convert2tif tests
     __assert_same_files_produced(params[cf.OBS_DIR], params_s[cf.OBS_DIR], "*_unw.tif", 17)
 
-    # prepifg tests
+    # prepifg + process steps that overwrite tifs test
     __assert_same_files_produced(params[cf.OUT_DIR], params_s[cf.OUT_DIR], "*cr.tif", 18)
 
     # TODO: timeseris and stack asserts
+    print("==========================xxx===========================")
 
     shutil.rmtree(params[cf.OBS_DIR])
     shutil.rmtree(params_s[cf.OBS_DIR])
