59f7a2a23b7bc1e9c2f80e58dd92f8953232b59a
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2020-05-05 13:54:23 +1000

    update openmpi default to 4.0.2; tidy up scripts and utils dirs

--- scripts/nci_install.sh
+++ scripts/nci_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -rf  ~/PyRateVenv
 rm -rf  ~/PyRate
-git clone https://github.com/GeoscienceAustralia/PyRate.git --branch file-handling --single-branch
+git clone https://github.com/GeoscienceAustralia/PyRate.git
 module purge
 module load python3/3.7.4
 module load gdal/3.0.2
-module load openmpi/2.1.6
+module load openmpi/4.0.2
 export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
 python3 -m venv ~/PyRateVenv
 source ~/PyRateVenv/bin/activate
--- scripts/nci_load_modules.sh
+++ scripts/nci_load_modules.sh
@@ -1,9 +1,9 @@
+#!bin/bash
 module purge
 module load python3/3.7.4
 module load gdal/3.0.2
-module load openmpi/2.1.6
+module load openmpi/4.0.2
 # Remove the prebuilt GDAL Python bindings that get added by the NCI module.
-#PYTHONPATH=`echo $PYTHONPATH | sed -e 's|/apps/gdal/.*:||'`
 export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
 # Required by Click
 export LC_ALL=en_AU.UTF-8
