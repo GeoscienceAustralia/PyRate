2f9dbbdafdb01ee0ec09280a9706eb8a4c639dc2
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2020-05-05 13:54:23 +1000

    update openmpi default to 4.0.2; tidy up scripts and utils dirs

--- scripts/create_html_documentation.sh
+++ scripts/create_html_documentation.sh
@@ -2,7 +2,7 @@
 module purge
 module load python3/3.7.4
 module load gdal/3.0.2
-module load openmpi/2.1.6
+module load openmpi/4.0.2
 export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
 source ~/PyRateVenv/bin/activate
 cd ~/PyRate/docs
--- scripts/nci_run_tests.sh
+++ scripts/nci_run_tests.sh
@@ -2,9 +2,9 @@
 module purge
 module load python3/3.7.4
 module load gdal/3.0.2
-module load openmpi/2.1.6
-export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPAT
+module load openmpi/4.0.2
+export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPATH
 source ~/PyRateVenv/bin/activate
 pip install -U pytest
 cd ~/PyRate
-pytest tests/test_algorithm
+pytest tests/
