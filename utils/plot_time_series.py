a899b08a604574f24575ba75b4a0c672843e5f77
Chandrakanta Ojha <Chandrakanta.Ojha@ga.gov.au>
2020-08-21 17:35:12 +1000

    Update plot_script with changes in function and include PyRate linear fit results to the time-series

--- utils/plot_time_series.py
+++ utils/plot_time_series.py
@@ -27,15 +27,17 @@ else:
     print(f"Looking for PyRate products in: {path}")
 
 # %% Calc model
-def calc_model(dph, imdates_ordinal, xvalues, model):
-    imdates_years = np.divide(imdates_ordinal,365.25) # imdates_ordinal / 365.25  ## dont care abs
+def calc_model(dph, imdates_ordinal, xvalues, model, vel1p, intercept1p):
+    imdates_years = np.divide(imdates_ordinal,365.25) # imdates_ordinal / 365.25
     xvalues_years = xvalues / 365.25
 
-    # models = ['Linear', 'Annual+L', 'Quad', 'Annual+Q']
     A = sm.add_constant(imdates_years)  # [1, t]
     An = sm.add_constant(xvalues_years)  # [1, t]
-    if model == 0:  # Linear
-        pass
+    if model == 0:  # PyRate linear fit results
+        # print(" M = ", vel1p, " & C = ", intercept1p )
+        A = np.dot(A, vel1p) + intercept1p
+        An = np.dot(An, vel1p) + intercept1p
+        # pass
     if model == 1:  # Annual+L
         sin = np.sin(2 * np.pi * imdates_years)
         cos = np.cos(2 * np.pi * imdates_years)
@@ -76,6 +78,12 @@ date_str = re.findall(r'\'(.+?)\'', ed)
 dac2 = xr.DataArray(vel[0,:,:], coords={'lon': x_coord2, 'lat': y_coord2}, dims=['lat', 'lon'])
 vs = xr.Dataset()
 vs['vel'] = dac2
+longitude = vs.coords['lon']
+latitude = vs.coords['lat']
+
+# linear intercept *tif file
+src = rasterio.open(os.path.join(path, 'linear_intercept.tif'))
+intercept = src.read()
 
 # pre-allocate a 3D numpy array to read the tscuml tiff raster bands to
 # include first 'zero' time slice, which PyRate does not save to disk
@@ -106,22 +114,6 @@ n_im, length, width = tscuml.shape
 # choose final time slice
 time_slice = len(imdates_dt)-1
 
-####  ANIMATION of displacement
-# import matplotlib.animation as animation
-# fig = plt.figure()
-# ims = []
-# for ii in range(1,time_slice+1):
-#     # print(ii)
-#     data = ds.tscuml[ii]
-#     cmap = plt.set_cmap('gist_rainbow')
-#     im = plt.imshow(data, animated=True)
-#     ims.append([im])
-
-# ani = animation.ArtistAnimation(fig, ims, interval=3, blit=True,
-#                                    repeat_delay=1000)
-# plt.show()
-#######
-
 
 ## set reference area and initial point
 refx1 = int(len(x_coord2)/ 2)
@@ -138,6 +130,8 @@ point_y = refy1
 # for last epoch
 auto_crange: float = 99.8
 refvalue_lastepoch = np.nanmean(tscuml[-1, refy1:refy2, refx1:refx2]) # reference values
+if str(refvalue_lastepoch) == 'nan':
+    refvalue_lastepoch = 0
 dmin_auto = np.nanpercentile((tscuml[-1, :, :]), 100 - auto_crange)
 dmax_auto = np.nanpercentile((tscuml[-1, :, :]), auto_crange)
 dmin = dmin_auto - refvalue_lastepoch
@@ -154,15 +148,13 @@ vmax = vmax_auto - refvalue_vel
 figsize = (7,7)
 pv = plt.figure('Velocity / Cumulative Displacement', figsize)
 axv = pv.add_axes([0.15,0.15,0.75,0.83])
-axv.set_title('Velocity')
+axv.set_title('Linear velocity')
 axt2 = pv.text(0.01, 0.99, 'Left-doubleclick:\n Plot time series\nRight-drag:\n Change ref area', fontsize=8, va='top')
 axt = pv.text(0.01, 0.78, 'Ref area:\n X {}:{}\n Y {}:{}\n (start from 0)'.format(refx1, refx2, refy1, refy2),
               fontsize=8, va='bottom')
 
 cmap = plt.set_cmap('bwr_r')
 cax = axv.imshow(vs.vel, clim=[vmin, vmax], cmap=cmap)
-#cax = axv.imshow(vs.vel, cmap, alpha=1, origin='upper',extent=[vs.coords['lon'].min(), vs.coords['lon'].max(), vs.coords['lat'].min(), vs.coords['lat'].max()], clim=(-100, 100)) #for displacement
-#cax = axv.imshow(ds.tscuml[1], cmap, alpha=1, origin='upper',extent=[ds.coords['lon'].min(), ds.coords['lon'].max(), ds.coords['lat'].min(), ds.coords['lat'].max()], clim=(-100, 100)) #for displacement
 cbr = pv.colorbar(cax, orientation='vertical')
 cbr.set_label('mm/yr')
 
@@ -171,16 +163,7 @@ mapdict_data = {}
 mapdict_unit = {}
 mapdict_vel = {'Vel': vel}
 mapdict_unit.update([('Vel', 'mm/yr')])
-mapdict_data = mapdict_vel  ## To move vel to top
-
-# names = ['Vel', 'Disp']
-# units = ['mm/yr', 'mm']
-# mapdict_data[names[0]] = vel
-# mapdict_unit[names[0]] = units[0]
-# mapdict_vel = {'Vel': vel}
-# mapdict_unit.update([('Vel', 'mm/yr')])
-# mapdict_vel = {'Vel': vel, 'Disp': ds.tscuml}
-# mapdict_unit.update([('Vel', 'mm/yr'), ('Disp', 'mm')])
+mapdict_data = mapdict_vel
 
 axrad_vel = pv.add_axes([0.01, 0.3, 0.13, len(mapdict_data)*0.025+0.04])
 ### Radio buttons
@@ -195,7 +178,6 @@ climauto = True
 def line_select_callback(eclick, erelease):
     global refx1, refx2, refy1, refy2, dmin, dmax   ## global cannot change existing values... why?
 
-    # refx1p, refx2p, refy1p, refy2p = refx1, refx2, refy1, refy2  ## Previous
     x1, y1 = eclick.xdata, eclick.ydata
     x2, y2 = erelease.xdata, erelease.ydata
 
@@ -208,11 +190,7 @@ def line_select_callback(eclick, erelease):
     elif y1 > y2:
         refy1, refy2 = [int(np.round(y1)), int(np.round(y2))]
 
-    # refx1h = refx1 - 0.5; refx2h = refx2 - 0.5  ## Shift half for plot
-    # refy1h = refy1 - 0.5; refy2h = refy2 - 0.5
-
     axt.set_text('Ref area:\n X {}:{}\n Y {}:{}\n (start from 0)'.format(refx1, refx2, refy1, refy2))
-    # rax.set_data([refx1h, refx2h, refx2h, refx1h, refx1h], [refy1h, refy1h, refy2h, refy2h, refy1h])
     pv.canvas.draw()
 
     ### Change clim
@@ -236,17 +214,14 @@ def line_select_callback(eclick, erelease):
 RS = RectangleSelector(axv, line_select_callback, drawtype='box', useblit=True, button=[3], spancoords='pixels', interactive=False)
 
 plt.connect('key_press_event', RS)
-# plt.show()
 
 
 vlimauto = True
-#val_ind = list(mapdict_data.keys())
 def show_vel(val_ind):
     global vmin, vmax, tscuml_disp_flag
     tscuml_disp_flag = False
 
     if 'Vel' in val_ind:  ## Velocity
-        # data = mapdict_data[val_ind[0]]
         data = mapdict_data[val_ind]
         if vlimauto:  ## auto
             vmin = np.nanpercentile(data, 100 - auto_crange)
@@ -254,15 +229,6 @@ def show_vel(val_ind):
         cax.set_cmap(cmap)
         cax.set_clim(vmin, vmax)
         cbr.set_label('mm/yr')
-
-    # if 'Disp' in val_ind:
-    #     data = mapdict_data[val_ind[1]][100]
-    #     if vlimauto:  ## auto
-    #         vmin = np.nanpercentile(data, 100 - auto_crange)
-    #         vmax = np.nanpercentile(data, auto_crange)
-    #     cax.set_cmap(cmap)
-    #     cax.set_clim(vmin, vmax)
-    #     cbr.set_label('mm/yr')
 
     cbr.set_label(mapdict_unit[val_ind])
     cax.set_data(data)
@@ -275,13 +241,17 @@ radio_vel.on_clicked(show_vel)
 
 # Slider for cumulative displacement
 axtim = pv.add_axes([0.1, 0.08, 0.8, 0.05], yticks=[])
-tslider = Slider(axtim, 'year', imdates_ordinal[0]-3, imdates_ordinal[-1]+3, valinit=imdates_ordinal[0])
-tslider.ax.bar(imdates_ordinal, np.ones(len(imdates_ordinal)), facecolor='black', width=4)
-tslider.ax.bar(imdates_ordinal[0], 1, facecolor='red', width=10)
+mdt = mdates.date2num(imdates_dt)
+tslider = Slider(axtim, 'year', mdates.date2num(imdates_dt[0]), mdates.date2num(imdates_dt[-1]))
+tslider.ax.bar(mdt, np.ones(len(mdt)), facecolor='black', width=4)
+tslider.ax.bar(mdt[0], 1, facecolor='red', width=12)
 
 loc_tslider = tslider.ax.xaxis.set_major_locator(mdates.AutoDateLocator())
 try:  # Only support from Matplotlib 3.1!
     tslider.ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(loc_tslider))
+    for label in tslider.ax.get_xticklabels():
+        label.set_rotation(20)
+        label.set_horizontalalignment('right')
 except:
     tslider.ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d'))
     for label in tslider.ax.get_xticklabels():
@@ -295,13 +265,12 @@ def tim_slidupdate(val):
     timenearest = np.argmin(np.abs(mdates.date2num(imdates_dt) - timein))
 
     dstr = imdates_dt[timenearest].strftime('%Y/%m/%d')
-    axv.set_title('%s (Ref: %s)' % (dstr, dstr_ref))
+    axv.set_title('Displacement map: %s (Ref: %s)' % (dstr, dstr_ref))
 
     newv = (tscuml[timenearest, :, :])
     cax.set_data(newv)
     cax.set_cmap(cmap)
     cax.set_clim(dmin, dmax)
-    # cax.set_clim(-100, 100)
     cbr.set_label('mm')
     tscuml_disp_flag = True
 
@@ -309,10 +278,8 @@ def tim_slidupdate(val):
 
 tslider.on_changed(tim_slidupdate)
 
-# point = plt.ginput() # for selecting a point
-
 ##### Plot figure of time-series at a point
-pts = plt.figure('Time-series')
+pts = plt.figure('Time-series', (9,5))
 axts = pts.add_axes([0.12, 0.14, 0.7, 0.8])
 
 axts.scatter(imdates_dt, np.zeros(len(imdates_dt)), c='b', alpha=0.6)
@@ -335,9 +302,11 @@ axtref = pts.text(0.83, 0.95, 'Ref area:\n X {}:{}\n Y {}:{}\n (start from 0)\nR
 
 ### Fit function for time series
 fitbox = pts.add_axes([0.83, 0.10, 0.16, 0.25])
-models = ['Linear', 'Annual+L', 'Quad', 'Annual+Q']
+models = ['PyRate linear rate', 'Annual+L model', 'Quad model', 'Annual+Q model']
 visibilities = [True, False, False, False]
 fitcheck = CheckButtons(fitbox, models, visibilities)
+for label in fitcheck.labels:
+    label.set_fontsize(8)
 
 def fitfunc(label):
     index = models.index(label)
@@ -355,8 +324,9 @@ pax2, = axv.plot([point_y], [point_x], 'Pk')
 ### Plot time series at clicked point
 lastevent = []
 geocod_flag = False
-# label1 = '1: No filter'
 label1 = 'tscuml'
+label2 = 'PyRate linear_rate'
+
 ylen = []
 
 def printcoords(event):
@@ -375,7 +345,7 @@ def printcoords(event):
     jj = np.int(np.round(event.xdata))
 
     ### Plot on image window
-    ii1h = ii - 0.5; ii2h = ii + 1 - 0.5  ## Shift half for plot
+    ii1h = ii - 0.5; ii2h = ii + 1 - 0.5
     jj1h = jj - 0.5; jj2h = jj + 1 - 0.5
 
     pax.set_data([jj1h, jj2h, jj2h, jj1h, jj1h], [ii1h, ii1h, ii2h, ii2h, ii1h])
@@ -395,7 +365,7 @@ def printcoords(event):
         val = mapdict_data[key][0, ii, jj]
         # val = val_temp[0,ii,jj]
         unit = mapdict_unit[key]
-        if key.startswith('Vel'):  ## Not plot here
+        if key.startswith('Vel'):
             continue
         elif key.startswith('n_') or key == 'mask':
             noisetxt = noisetxt + '{}: {:d} {}\n'.format(key, int(val), unit)
@@ -412,7 +382,8 @@ def printcoords(event):
 
     ### If not masked
     ### tscuml file
-    vel1p = vel[0, ii, jj] #- np.nanmean((vel[0,refy1:refy2, refx1:refx2]))
+    vel1p = vel[0, ii, jj]
+    intercept1p = intercept[0,ii,jj]
     dph = tscuml[:,ii,jj]
 
     ## fit function
@@ -420,13 +391,16 @@ def printcoords(event):
     xvalues = np.arange(imdates_ordinal[0], imdates_ordinal[-1], 10)
     xdates = [dt.fromordinal(pp) for pp in xvalues]
     for model, vis in enumerate(visibilities):
-        yvalues = calc_model(dph, imdates_ordinal, xvalues, model)
-        #lines1[model], = axts.plot(xvalues, yvalues, 'r-', visible=vis, alpha=0.6, zorder=3)
-        lines1[model], = axts.plot(xdates, yvalues, 'r-', visible=vis, alpha=0.6, zorder=3)
+        yvalues = calc_model(dph, imdates_ordinal, xvalues, model, vel1p, intercept1p)
+        if model == 0:
+            lines1[model], = axts.plot(xdates, yvalues, 'r-', label=label2, visible=vis, alpha=0.6, zorder=3)
+            axts.legend()
+        else:
+            lines1[model], = axts.plot(xdates, yvalues, 'g-', visible=vis, alpha=0.6, zorder=3)
 
     axts.scatter(imdates_dt, dph, label=label1, c='b', alpha=0.6, zorder=5)
     axts.set_title('vel = {:.1f} mm/yr @({}, {})'.format(vel1p, jj, ii), fontsize=10)
-    # axts.set_ylim(-100,100) # Chandra added
+    # axts.set_ylim(-100,100)
 
     ### Y axis
     if ylen:
