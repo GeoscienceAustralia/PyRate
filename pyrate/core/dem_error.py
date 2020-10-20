calculation of metric height difference w.r.t. DEM, correct handling of DEM error correction in case it has already been calculated in a previous run, inclusion of new switch parameter 'demerror' in the config file and a threshold parameter 'de_pthr' for the number of valid pixels in the time series

pyrate/core/dem_error.py
@@ -26,8 +26,7 @@ from numpy.linalg import inv, LinAlgError
 from pyrate.core import geometry, shared, mpiops, config as cf, ifgconstants as ifc
 from pyrate.core.logger import pyratelogger as log
 from pyrate.core.shared import Ifg, Geometry
-from pyrate.configuration import Configuration
-from pyrate.configuration import MultiplePaths
+from pyrate.configuration import Configuration, MultiplePaths
 from pyrate.merge import assemble_tiles
 
 
@@ -42,13 +41,19 @@ def dem_error_calc_wrapper(params: dict) -> None:
         return
     # geometry information needed to calculate Bperp for each pixel using first IFG in list
     ifg_paths = [ifg_path.tmp_sampled_path for ifg_path in params[cf.INTERFEROGRAM_FILES]]
-    ifg0_path = ifg_paths[0]
-    ifg0 = Ifg(ifg0_path)
-    ifg0.open(readonly=True)
 
-    # not currently implemented for ROIPAC data which breaks some tests
-    # if statement can be deleted once ROIPAC is deprecated from PyRate
-    if not ifg0.meta_data[ifc.PYRATE_INSAR_PROCESSOR] == 'ROIPAC':
+    # check if DEM error correction is already available
+    if mpiops.run_once(__check_and_apply_demerrors_found_on_disc, ifg_paths, params):
+        log.warning("Reusing DEM error correction from previous run!!!")
+    else:
+      # read and open the first IFG in list
+      ifg0_path = ifg_paths[0]
+      ifg0 = Ifg(ifg0_path)
+      ifg0.open(readonly=True)
+
+      # not currently implemented for ROIPAC data which breaks some tests
+      # if statement can be deleted once ROIPAC is deprecated from PyRate
+      if not ifg0.meta_data[ifc.PYRATE_INSAR_PROCESSOR] == 'ROIPAC':
 
         if params[cf.BASE_FILE_LIST] is None:
             msg = f"No baseline files supplied: DEM error correction not computed"
@@ -78,6 +83,7 @@ def dem_error_calc_wrapper(params: dict) -> None:
         params[cf.TILES] = Configuration.get_tiles(params)
         tiles = params[cf.TILES]
         preread_ifgs = params[cf.PREREAD_IFGS]
+        # todo: subtract other corrections (e.g. orbital) from displacement phase before estimating the DEM error
         vcmt = params[cf.VCMT]
         threshold = params[cf.DE_PTHR]
 
@@ -85,12 +91,11 @@ def dem_error_calc_wrapper(params: dict) -> None:
         lon, lat = geometry.get_lonlat_coords(ifg0)
         # cut rg and az to tile size
 
-    # the following code is a quick way to do the bperp calculation, but is not identical to the GAMMA output
-    # where the near range of the first SLC is used for each pair.
-    # calculate look angle for interferograms (using the Near Range of the first SLC)
-    #look_angle = geometry.calc_local_geometry(ifg0, None, rg, lon, lat, params)
-    #bperp = geometry.calc_local_baseline(ifg0, az, look_angle, params)
-    #print(bperp.shape)
+        # the following code is a quick way to do the bperp calculation, but is not identical to the GAMMA output
+        # where the near range of the first SLC is used for each pair.
+        # calculate look angle for interferograms (using the Near Range of the first SLC)
+        #look_angle = geometry.calc_local_geometry(ifg0, None, rg, lon, lat, params)
+        #bperp = geometry.calc_local_baseline(ifg0, az, look_angle, params)
 
         # process in tiles
         process_tiles = mpiops.array_split(tiles)
@@ -109,14 +114,13 @@ def dem_error_calc_wrapper(params: dict) -> None:
                 ifg = Ifg(ifg_path)
                 ifg.open(readonly=True)
                 # calculate look angle for interferograms (using the Near Range of the primary SLC)
-                look_angle = geometry.calc_local_geometry(ifg, None, rg_parts, lon_parts, lat_parts, params)
+                look_angle, range_dist = geometry.calc_local_geometry(ifg, None, rg_parts, lon_parts, lat_parts, params)
                 bperp[ifg_num, :, :] = geometry.calc_local_baseline(ifg, az_parts, look_angle, params)
                 ifg_num += 1
 
-
             log.debug('Calculating DEM error for tile {} during DEM error correction'.format(t.index))
             #mst_tile = np.load(Configuration.mst_path(params, t.index))
-            dem_error, dem_error_correction = calc_dem_errors(ifg_parts, bperp, threshold, vcmt)
+            dem_error, dem_error_correction = calc_dem_errors(ifg_parts, bperp, look_angle, range_dist, threshold, vcmt)
             # dem_error contains the estimated DEM error for each pixel (i.e. the topographic change relative to the DEM)
             # size [row, col]
             # dem_error_correction contains the correction value for each interferogram
@@ -141,26 +145,25 @@ def dem_error_calc_wrapper(params: dict) -> None:
     log.info('Finished DEM error correction')
 
 
-def calc_dem_errors(ifgs, bperp, threshold, vcmt):
+def calc_dem_errors(ifgs, bperp, look_angle, range_dist, threshold, vcmt):
     """
     Calculates the per-pixel DEM error using least-squares adjustment of phase data and
     perpendicular baseline. The least-squares adjustment co-estimates the velocities.
 
         - *nrows* is the number of rows in the ifgs,
         - *ncols* is the  number of columns in the ifgs, and
-        - *nepochs* is the number of unique epochs (dates)
 
     :param list ifgs: list of interferogram class objects.
-    :param dict params: Dictionary of configuration parameters
     :param ndarray bperp: Per-pixel perpendicular baseline for each interferogram
+    :param ndarray look_angle: Per-pixel look angle
+    :param ndarray range_dist: Per-pixel range distance measurement
+    :param int threshold: minimum number of redundant phase values at pixel (config parameter de_pthr)
     :param ndarray vcmt: Positive definite temporal variance covariance matrix
-    :param ndarray mst: [optional] Minimum spanning tree array
 
     :return: ndarray dem_error: estimated per-pixel dem error (nrows x ncols)
     :return: ndarray dem_error_correction: DEM error correction for each pixel and interferogram (nifgs x nrows x ncols)
     """
     ifg_data, mst, ncols, nrows, bperp_data, ifg_time_span = _perpixel_setup(ifgs, bperp)
-    nifgs = ifg_data.shape[0]
     if threshold < 4:
         msg = f"pixel threshold too low (i.e. <4) resulting in singularities in DEM error estimation"
         raise DEMError(msg)
@@ -171,19 +174,22 @@ def calc_dem_errors(ifgs, bperp, threshold, vcmt):
     for row in range(nrows):
         for col in range(ncols):
             # calc DEM error for each pixel with valid Bperp and ifg phase data
-            # todo use a threshold for the minimum number of ifgs per pixel
             # check pixel for non-redundant ifgs
             sel = np.nonzero(mst[:, row, col])[0]  # trues in mst are chosen
-            if len(sel) >= threshold:
+            if len(sel) >= threshold: # given threshold for number of valid pixels in time series
                 ifgv = ifg_data[sel, row, col]
                 bperp_pix = bperp_data[sel, row, col]
+                #geom = bperp_pix / (range_dist[row, col] * np.sin(look_angle[row, col]))
                 time_span = ifg_time_span[sel]
                 # new covariance matrix using actual number of observations
                 m = len(sel)
-                Qyy = np.eye(m)
+                Qyy = np.eye(m) # in case the weights are not used -> linalg.lstsq can be used instead
+                # results get unstable when using the VCM information -> todo: why?
                 #Qyy[:m, :m] = vcmt[sel, np.vstack(sel)]
                 # Design matrix of least-squares system
-                A = np.column_stack((np.ones(m), bperp_pix , time_span))
+                A = np.column_stack((np.ones(m), bperp_pix, time_span))
+                #A = np.column_stack((np.ones(m), geom , time_span))
+                # displacement observations (in mm)
                 y = np.vstack(ifgv)
                 # solve weighted least-squares system (not available in scipy!)
                 try:
@@ -195,7 +201,12 @@ def calc_dem_errors(ifgs, bperp, threshold, vcmt):
                 except LinAlgError as err: # nan value for DEM error in case of singular Qyy matrix
                     if 'Singular matrix' in str(err):
                         dem_error[row][col] = np.nan
+
+    # calculate correction value for each IFG by multiplying the least-squares estimate with the Bperp value
     dem_error_correction = np.multiply(dem_error, bperp_data)
+    # calculate metric difference to DEM by multiplying the estimate with the per-pixel geometry
+    # (i.e. range distance and look angle, see Eq. (2.4.12) in Hanssen (2001)) and scaling by 0.001 (obs are in mm)
+    dem_error = np.multiply(dem_error, np.multiply(range_dist, np.sin(look_angle))) * 0.001
 
     return dem_error, dem_error_correction
 
@@ -246,13 +257,47 @@ def _write_dem_errors(ifg_paths, params, preread_ifgs, tiles):
     idx = 0
     for ifg_path in ifg_paths:
         ifg = Ifg(ifg_path)
-        ifg.open(readonly=True)
+        ifg.open()
         # read dem error correction file from tmpdir (size
         dem_error_correction_ifg = assemble_tiles(shape, params[cf.TMPDIR], tiles, out_type='dem_error_correction', \
                                                   index=idx)
         idx += 1
         dem_error_correction_on_disc = MultiplePaths.dem_error_path(ifg.data_path, params)
         np.save(file=dem_error_correction_on_disc, arr=dem_error_correction_ifg)
+
+        # subtract DEM error from the ifg
+        ifg.phase_data -= dem_error_correction_ifg
+        _save_dem_error_corrected_phase(ifg)
+
+
+def __check_and_apply_demerrors_found_on_disc(ifg_paths, params):
+    """
+    Convenience function to check if DEM error correction files have already been produced in a previous run
+    """
+    saved_dem_err_paths = [MultiplePaths.dem_error_path(ifg_path, params) for ifg_path in ifg_paths]
+    for d, i in zip(saved_dem_err_paths, ifg_paths):
+        if d.exists():
+            dem_corr = np.load(d)
+            if isinstance(i, str):
+                # are paths
+                ifg = Ifg(i)
+                ifg.open()
+            else:
+                ifg = i
+            ifg.phase_data -= dem_corr
+            # set orbfit meta tag and save phase to file
+            _save_dem_error_corrected_phase(ifg)
+    return all(d.exists() for d in saved_dem_err_paths)
+
+
+def _save_dem_error_corrected_phase(ifg):
+    """
+    Convenience function to update metadata and save latest phase after DEM error correction
+    """
+    # set orbfit tags after orbital error correction
+    ifg.dataset.SetMetadataItem(ifc.PYRATE_DEM_ERROR, ifc.DEM_ERROR_REMOVED)
+    ifg.write_modified_phase()
+    ifg.close()
 
 
 def _remove_file_if_exists(file):

pyrate/core/gamma.py
@@ -533,45 +533,46 @@ def read_lookup_table(head, data_path, xlooks, ylooks, xmin, xmax, ymin, ymax):
         lt_data_rg = np.empty((nrows, ncols)) * np.nan # nan array with size of input data set
 
     else: # this is a real data set with an lt-file of correct size
-      row_bytes = ncols_lt * 2 * bytes_per_col
-      lt_data_az = np.empty((0, ncols))  # empty array with correct number of columns
-      lt_data_rg = np.empty((0, ncols))  # empty array with correct number of column
-      # for indexing: lookup table file contains value pairs (i.e. range, azimuth)
-      # value pair 0 would be index 0 and 1, value pair 1 would be index 2 and 3, and so on
-      # example: for a multi-looking factor of 10 we want value pair 4, 14, 24, ...
-      # this would be index 8 and 9, index 28 and 29, 48 and 49, ...
-      # start column needs to be added in case cropping is applied
-      if xlooks == 1:
+        row_bytes = ncols_lt * 2 * bytes_per_col
+        lt_data_az = np.empty((0, ncols))  # empty array with correct number of columns
+        lt_data_rg = np.empty((0, ncols))  # empty array with correct number of column
+        # for indexing: lookup table file contains value pairs (i.e. range, azimuth)
+        # value pair 0 would be index 0 and 1, value pair 1 would be index 2 and 3, and so on
+        # example: for a multi-looking factor of 10 we want value pair 4, 14, 24, ...
+        # this would be index 8 and 9, index 28 and 29, 48 and 49, ...
+        # start column needs to be added in case cropping is applied
+        if (xlooks % 2) == 0:  # for even ml factors
+            idx_start = xmin + int(xlooks / 2) - 1
+        else: # for odd ml factors
+            idx_start = xmin + int((xlooks - 1) / 2)
         # old code not working for cropped data sets:
-        #idx_start = 0
-        idx_start = xmin
-      else:
+        #idx_rg = np.arange(idx_start, ncols_lt*2, 2*xlooks) # first value
+        #idx_az = np.arange(idx_start+1, ncols_lt*2, 2*xlooks) # second value
+        idx_rg = np.arange(2 * idx_start, xmax * 2, 2 * xlooks)  # first value
+        idx_az = np.arange(2 * idx_start + 1, xmax * 2, 2 * xlooks)  # second value
+        # row index used (e.g. for multi-looking factor 10: 4, 14, 24, ...)
         # old code not working for cropped data sets:
-        #idx_start = (int(xlooks / 2) - 1) * 2
-        idx_start = (xmin + int(xlooks / 2) - 1) * 2
-      # old code not working for cropped data sets:
-      #idx_rg = np.arange(idx_start, ncols_lt*2, 2*xlooks) # first value
-      #idx_az = np.arange(idx_start+1, ncols_lt*2, 2*xlooks) # second value
-      idx_rg = np.arange(idx_start, xmax * 2, 2 * xlooks)  # first value
-      idx_az = np.arange(idx_start + 1, xmax * 2, 2 * xlooks)  # second value
-      # row index used (e.g. for multi-looking factor 10: 4, 14, 24, ...)
-      # old code not working for cropped data sets:
-      #row_idx = np.arange(int(ylooks/2)-1, nrows_lt, ylooks)
-      row_idx = np.arange(ymin + int(ylooks / 2) - 1, ymax, ylooks)
+        #row_idx = np.arange(int(ylooks/2)-1, nrows_lt, ylooks)
+        # set up row idx, e.g. for ml=10: 4, 14, 24, ...
+        if (ylooks % 2) == 0: # for even ml factors
+            idx_start = ymin + int(ylooks / 2) - 1
+        else: # for odd ml factors
+            idx_start = ymin + int((ylooks - 1) / 2)
+        row_idx = np.arange(idx_start, ymax, ylooks)
 
-      # read the binary lookup table file and save the range/azimuth value pair for each position in ML data
-      log.debug(f"Reading lookup table file {data_path}")
-      with open(data_path, 'rb') as f:
-        for y in range(nrows_lt): # loop through all lines in file
-            # this could potentially be made quicker by skipping unwanted bytes in the f.read command?
-            data = struct.unpack(fmtstr, f.read(row_bytes))
-            # but only read data from lines in row index:
-            if y in row_idx:
-                row_data = np.array(data)
-                row_data_ml_az = row_data[idx_az] # azimuth for PyRate
-                row_data_ml_rg = row_data[idx_rg] # range for PyRate
-                lt_data_az = np.append(lt_data_az, [row_data_ml_az], axis=0)
-                lt_data_rg = np.append(lt_data_rg, [row_data_ml_rg], axis=0)
+        # read the binary lookup table file and save the range/azimuth value pair for each position in ML data
+        log.debug(f"Reading lookup table file {data_path}")
+        with open(data_path, 'rb') as f:
+            for y in range(nrows_lt): # loop through all lines in file
+                # this could potentially be made quicker by skipping unwanted bytes in the f.read command?
+                data = struct.unpack(fmtstr, f.read(row_bytes))
+                # but only read data from lines in row index:
+                if y in row_idx:
+                    row_data = np.array(data)
+                    row_data_ml_az = row_data[idx_az] # azimuth for PyRate
+                    row_data_ml_rg = row_data[idx_rg] # range for PyRate
+                    lt_data_az = np.append(lt_data_az, [row_data_ml_az], axis=0)
+                    lt_data_rg = np.append(lt_data_rg, [row_data_ml_rg], axis=0)
 
     return lt_data_az, lt_data_rg
 

pyrate/core/geometry.py
@@ -102,16 +102,16 @@ def calc_local_geometry(ifg, ifg_path, rg, lon, lat, params):
                            np.square(a * np.cos(lat)) + np.square(b * np.sin(lat))))
 
     # range measurement at pixel ij
-    range_ij = near_range + rps * rg
+    range_dist = near_range + rps * rg
 
     # look angle at pixel ij -> law of cosines in "satellite - Earth centre - ground pixel" triangle
     # see e.g. Section 2 in https://www.cs.uaf.edu/~olawlor/ref/asf/sar_equations_2006_08_17.pdf
-    look_angle = np.arccos(np.divide(se**2 + np.square(range_ij) - np.square(re), 2 * se * range_ij))
+    look_angle = np.arccos(np.divide(se**2 + np.square(range_dist) - np.square(re), 2 * se * range_dist))
 
     # incidence angle at pixel ij -> law of cosines in "satellite - Earth centre - ground pixel" triangle
     # see e.g. Section 2 in https://www.cs.uaf.edu/~olawlor/ref/asf/sar_equations_2006_08_17.pdf
-    incidence_angle = np.pi - np.arccos(np.divide(np.square(range_ij) + np.square(re) - se**2, \
-                                                  2 * np.multiply(range_ij, re)))
+    incidence_angle = np.pi - np.arccos(np.divide(np.square(range_dist) + np.square(re) - se**2, \
+                                                  2 * np.multiply(range_dist, re)))
 
     # todo (once new test data is ready): move next line into test for validation with GAMMA output
     #incidence_angle_gamma = np.pi / 2 - incidence_angle
@@ -175,7 +175,7 @@ def calc_local_geometry(ifg, ifg_path, rg, lon, lat, params):
         remove_file_if_exists(azimuth_angle_file)
         shared.write_output_geotiff(md, gt, wkt, azimuth_angle, azimuth_angle_file, np.nan)
 
-    return look_angle
+    return look_angle, range_dist
 
 
 def calc_local_baseline(ifg, az, look_angle, params):