98dd5405c69632506b4f59a3027942f837f4fcfb
Sudipta Basak <basaks@gmail.com>
2020-10-23 06:19:35 +1100

    filter to list of ifgs used in all closed loops

--- tests/test_data/small_test/tif/generate_sythetic_unws.py
+++ tests/test_data/small_test/tif/generate_sythetic_unws.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+import itertools
+from typing import List, Dict
 from datetime import datetime, date
 import numpy as np
 import networkx as nx
@@ -23,9 +25,10 @@ def generate_random_edges(ifgs):
 
     secondary = [date(y, m, d) for y, m, d in zip(year, month, day)]
 
-    weights = np.random.rand(num) * 0.1
-    edges_with_weights = [(i.first, i.second, i.nan_fraction) for i in ifgs]
-    original_loopifgs = [LoopIfgMock(i.first, i.second, i.nan_fraction) for i in ifgs]
+    # TODO: support ifg weights
+    weights = np.ones_like(day)
+    edges_with_weights = [(i.first, i.second, 1) for i in ifgs]
+    original_loopifgs = [LoopIfgMock(i.first, i.second, 1) for i in ifgs]
     all_edges = edges_with_weights + [(p, s, w) for p, s, w in zip(primary, secondary, weights)]
     all_ifgs = original_loopifgs + [LoopIfgMock(p, s, w) for p, s, w in zip(primary, secondary, weights)]
     return all_edges, all_ifgs
@@ -38,10 +41,36 @@ def find_closed_loops(all_edges):
     dg = nx.DiGraph(g)
     simple_cycles = nx.simple_cycles(dg)  # will have all edges
     simple_cycles = [scc for scc in simple_cycles if len(scc) > 2]  # discard edges
+    # TODO: also discard loops when the loop members are the same?
     return simple_cycles
 
+
+def associate_ifgs_with_loops(loops):
+    loops_to_ifgs = {}
+    for i, l in enumerate(loops):
+        loop_ifgs = []
+        for ii, _ in enumerate(l[:-1]):
+            ifg = LoopIfgMock(l[ii], l[ii+1], 1)
+            loop_ifgs.append(ifg)
+
+        ifg = LoopIfgMock(l[ii+1], l[0], 1)
+        loop_ifgs.append(ifg)  # add the last to the first node, i.e., close the loop
+        loops_to_ifgs[i] = loop_ifgs
+
+    return loops_to_ifgs
+
+
+def filter_to_ifgs_used_in_loops(loops_to_ifgs: Dict[(int, LoopIfgMock)]):
+    return set(itertools.chain(*loops_to_ifgs.values()))
+
+
+def create_mock_ifgs_data_unless_already_available():
+    pass
 
 _, ifgs = common.copy_and_setup_small_data()
 all_edges, all_mock_ifgs = generate_random_edges(ifgs)
 all_loops = find_closed_loops(all_edges)
+loops_to_ifgs = associate_ifgs_with_loops(all_loops)
+ifgs_used = filter_to_ifgs_used_in_loops(loops_to_ifgs)
 
+import IPython; IPython.embed()
