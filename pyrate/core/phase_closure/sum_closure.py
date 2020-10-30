bfca3c1920ce95a0e356f6ebe6ab5fc213627621
Sudipta Basak <basaks@gmail.com>
2020-10-31 08:45:14 +1100

    threshold checking for pixels with large deviations

--- pyrate/core/phase_closure/sum_closure.py
+++ pyrate/core/phase_closure/sum_closure.py
@@ -6,7 +6,7 @@ from pyrate.core.shared import Ifg, dem_or_ifg
 from pyrate.core.phase_closure.mst_closure import Edge, WeightedEdge, SignedEdge, mst_closure_wrapped
 
 THRESHOLD_TO_REMOVE_PIXEL = 0.25
-LARGE_DEVIATION_THRESHOLD_FOR_PIXEL = 0.25
+LARGE_DEVIATION_THRESHOLD_FOR_PIXEL = 3.14152  # pi
 THRESHOLD_TO_REMOVE_IFG = 0.5
 MAX_LOOP_LENGTH = 5  # loops upto this many edges are considered for closure checks
 IndexedIfg = namedtuple('IndexedIfg', ['index', 'Ifg'])
@@ -21,8 +21,7 @@ def create_ifg_edge_dict(ifg_files) -> Dict[Edge, IndexedIfg]:
     return {Edge(ifg.first, ifg.second): IndexedIfg(index, ifg) for index, ifg in enumerate(ifgs)}
 
 
-def sum_phase_values_for_each_loop(ifg_files: List[str], loops: List[List[SignedEdge]],
-                                   threshold: float = None):
+def sum_phase_values_for_each_loop(ifg_files: List[str], loops: List[List[SignedEdge]], threshold: float = None):
     edge_to_indexed_ifgs = create_ifg_edge_dict(ifg_files)
     ifgs = [v.Ifg for v in edge_to_indexed_ifgs.values()]
     n_ifgs = len(ifgs)
@@ -37,21 +36,26 @@ def sum_phase_values_for_each_loop(ifg_files: List[str], loops: List[List[Signed
     for k, loop in enumerate(loops):
         for signed_edge in loop:
             ifg = edge_to_indexed_ifgs[signed_edge.edge].Ifg
-            index = edge_to_indexed_ifgs[signed_edge.edge].index
+            index = edge_to_indexed_ifgs[signed_edge.edge].index  # iline
             closure[:, :, k] += signed_edge.sign * ifg.phase_data
             ifg_num[index] += 1
+
         closure[:, :, k] -= np.nanmedian(closure[:, :, k])
 
-    if threshold:
-        pass
+        if threshold:
+            indices_breaching_threshold = np.absolute(closure[:, :, k] > threshold)
+            for signed_edge in loop:
+                index = edge_to_indexed_ifgs[signed_edge.edge].index  # iline
+                #  the variable check_ps is increased by 1 for that pixel
+                check_ps[indices_breaching_threshold, index] += 1
 
-    return closure
+    return closure, check_ps
 
 
 new_test_files = Path('/home/sudipta/Documents/GEOTIFF').glob('*_unw.tif')
 ifg_files = [f.as_posix() for f in new_test_files]
 signed_loops = mst_closure_wrapped(ifg_files=ifg_files)
 retained_loops = [sl for sl in signed_loops if len(sl) <= MAX_LOOP_LENGTH]
-closure = sum_phase_values_for_each_loop(ifg_files, retained_loops)
+closure, check_ps = sum_phase_values_for_each_loop(ifg_files, retained_loops, LARGE_DEVIATION_THRESHOLD_FOR_PIXEL)
 import IPython; IPython.embed()
 
