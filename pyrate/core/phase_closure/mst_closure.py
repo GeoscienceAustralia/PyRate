32903e6860950054e2e50751515412b613cc8748
Sudipta Basak <basaks@gmail.com>
2020-12-22 12:53:56 +1100

    weighed signed edges implemented and sorted based on duration of loops, followed by first start date

--- pyrate/core/phase_closure/mst_closure.py
+++ pyrate/core/phase_closure/mst_closure.py
@@ -9,6 +9,20 @@ SignedEdge = namedtuple('SignedEdge', ['edge', 'sign'])
 SignedWeightedEdge = namedtuple('SignedWeightedEdge', ['SignedEdge', 'weight'])
 
 
+class WeightedLoop:
+
+    def __init__(self, loop: List[SignedWeightedEdge]):
+        self.loop = loop
+
+    @property
+    def weight(self):
+        return sum({l.weight for l in self.loop})
+
+    @property
+    def earliest_date(self):
+        return min({loop.SignedEdge.edge.first for loop in self.loop})
+
+
 def discard_edges_with_same_members(simple_cycles):
     seen_sc_sets = set()
     filtered_sc = []
@@ -49,7 +63,10 @@ def add_signs_and_weights_to_loops(loops, available_edges) -> List[List[SignedEd
                 edge = Edge(l[ii+1], ll)
                 assert edge in available_edges
                 signed_edge = SignedEdge(edge, -1)  # in direction of ifg
-            weighted_signed_edge = SignedWeightedEdge(signed_edge, signed_edge.edge.second - signed_edge.edge.first)
+            weighted_signed_edge = SignedWeightedEdge(
+                signed_edge,
+                (signed_edge.edge.second - signed_edge.edge.first).days
+            )
             weighted_signed_loop.append(weighted_signed_edge)
 
         weighted_signed_loops.append(weighted_signed_loop)
@@ -72,3 +89,9 @@ def find_signed_closed_loops(ifg_files: List[str]) -> List[List[SignedEdge]]:
     signed_loops = add_signs_and_weights_to_loops(all_loops, available_edges)
     signed_loops.sort(key=lambda x: x.weight)
     return signed_loops
+
+
+def sort_loops_based_on_weights_and_date(signed_loops: List[List[SignedEdge]]) -> List[WeightedLoop]:
+    weighted_loops = [WeightedLoop(sl) for sl in signed_loops]
+    weighted_loops.sort(key=lambda x: [x.weight, x.earliest_date])  # sort based on weights and date
+    return weighted_loops
--- tests/phase_closure/test_mst_closure.py
+++ tests/phase_closure/test_mst_closure.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+import numpy as np
 import pytest
 from pyrate.constants import PYRATEPATH
 from pyrate.core.phase_closure.mst_closure import (
     find_closed_loops, Edge, SignedWeightedEdge, SignedEdge, setup_edges,
-    add_signs_and_weights_to_loops,
+    add_signs_and_weights_to_loops, sort_loops_based_on_weights_and_date
 )
 
 GEOTIFF = PYRATEPATH.joinpath('tests', 'test_data', 'geotiffs')
@@ -51,3 +52,20 @@ def test_associate_ifgs_with_loops(signed_loops, geotiffs):
     assert isinstance(signed_loops[0][0], SignedWeightedEdge)
     assert isinstance(signed_loops[0][0][0], SignedEdge)
     assert isinstance(signed_loops[0][0][0][0], Edge)
+
+
+def test_sort_loops_based_on_weights_and_date(signed_loops, geotiffs):
+    weighted_loops = sort_loops_based_on_weights_and_date(signed_loops)
+    assert len(weighted_loops) == 541
+    # order
+    weights = [w.weight for w in weighted_loops]
+    earliest_dates = [w.earliest_date for w in weighted_loops]
+    assert np.all(np.diff(weights) >= 0)
+
+    for i, (w, d) in enumerate(zip(weights[:-1], earliest_dates[:-1])):
+        sub_list = [d]
+        if w == weights[i+1]:
+            sub_list.append(earliest_dates[i+1])
+        if len(sub_list) > 1:
+            tds = np.array([td.days for td in np.diff(sub_list)])
+            assert np.all( tds >= 0)  # assert all dates are increasing for same weights
