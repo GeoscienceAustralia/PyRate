58254b7210a70f268e782c849b7ce8577465343d
Sudipta Basak <basaks@gmail.com>
2020-12-23 14:04:13 +1100

    fix plot index

--- pyrate/core/phase_closure/plot_closure.py
+++ pyrate/core/phase_closure/plot_closure.py
@@ -22,19 +22,19 @@ def plot_closure(closure: np.ndarray, loops: List[WeightedLoop]):
     if n_loops % plt_rows:
         plt_cols += 1
 
-    fig = plt.figure(figsize=(6*plt_rows, 4*plt_cols))
+    fig = plt.figure(figsize=(12*plt_rows, 8*plt_cols))
 
     tot_plots = 1
     for p_r in range(plt_rows):
         for p_c in range(plt_cols):
             ax = fig.add_subplot(plt_rows, plt_cols, tot_plots)
-            data = closure[:, :, p_r * (p_c-1) + p_c]
-            loop = loops[p_r * (p_c-1) + p_c]
+            data = closure[:, :, plt_cols * p_r + p_c]
+            loop = loops[plt_cols * p_r + p_c]
             leg = ',\n'.join([swe.SignedEdge.edge.first.isoformat() + '-' + swe.SignedEdge.edge.second.isoformat()
                               for swe in loop.loop])
             im = ax.imshow(data, vmin=-PI/2, vmax=PI/2, cmap=cmap)
-            text = ax.text(10, 10, leg, bbox={'facecolor': 'white', 'pad': 5})
-            text.set_fontsize(min(10, int(n_loops/10)))
+            text = ax.text(20, 20, leg, bbox={'facecolor': 'white', 'pad': 5})
+            text.set_fontsize(min(20, int(n_loops/5)))
 
             divider = make_axes_locatable(ax)
             cax = divider.append_axes("right", size="5%", pad=0.05)
