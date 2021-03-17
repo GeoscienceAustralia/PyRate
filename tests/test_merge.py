993964601a744617cdad23be6b79976b5e9f8fa9
Sudipta Basak <basaks@gmail.com>
2021-03-17 19:56:29 +1100

    reuse fixtures during test

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -42,28 +42,11 @@ def los_projection(request):
     return request.param
 
 
-@pytest.fixture(scope='module')
-def create_pre_merge_output():
-    tdir = Path(tempfile.mkdtemp())
-    params = manipulate_test_conf(MEXICO_CROPA_CONF, tdir)
-    output_conf_file = tdir.joinpath('conf.cfg')
-    output_conf = tdir.joinpath(output_conf_file)
-    write_config_file(params=params, output_conf_file=output_conf)
-    check_call(f"mpirun -n 2 pyrate conv2tif -f {output_conf}", shell=True, env=os.environ)
-    check_call(f"mpirun -n 2 pyrate prepifg -f {output_conf}", shell=True, env=os.environ)
-    check_call(f"mpirun -n 2 pyrate correct -f {output_conf}", shell=True, env=os.environ)
-    check_call(f"mpirun -n 2 pyrate timeseries -f {output_conf}", shell=True, env=os.environ)
-    check_call(f"mpirun -n 2 pyrate stack -f {output_conf}", shell=True, env=os.environ)
-
-    params = Configuration(output_conf).__dict__
-    return params
-
-
 def test_los_conversion_divisors():
     """
     Unit test to check the LOS conversions for specific incidence angles
     """
-    inc = [0, 30, 45, 90] # incidence angles in degrees
+    inc = [0, 30, 45, 90]  # incidence angles in degrees
 
     # Test pseudo-vertical
     res = [los_projection_divisors[ifc.PSEUDO_VERTICAL](np.radians(x)) for x in inc]
@@ -83,88 +66,104 @@ def test_los_conversion_divisors():
 
 @pytest.mark.mpi
 @pytest.mark.slow
-# @pytest.mark.skipif(not PY37GDAL302, reason="Only run in one CI env")
-def test_los_conversion_comparison(create_pre_merge_output):
-    """
-    compare outputs in each of the los projection types
-    compares sine and cosine components are larger than LOS component
-    assert metadata equal except
-    """
-    params = create_pre_merge_output
-    all_dirs = {}
-    for k in LOS_PROJECTION_OPTION.keys():
-        params[C.LOS_PROJECTION] = k
-        _merge_stack(params)
-        _merge_linrate(params)
-        _merge_timeseries(params, 'tscuml')
-        _merge_timeseries(params, 'tsincr')
-        k_dir = Path(params[C.OUT_DIR]).joinpath(ifc.LOS_PROJECTION_OPTION[k])
-        k_dir.mkdir(exist_ok=True)
+@pytest.mark.skipif(not PY37GDAL302, reason="Only run in one CI env")
+class TestLOSConversion:
+    @classmethod
+    def setup_class(cls):
+        tdir = Path(tempfile.mkdtemp())
+        params = manipulate_test_conf(MEXICO_CROPA_CONF, tdir)
+        output_conf_file = tdir.joinpath('conf.cfg')
+        output_conf = tdir.joinpath(output_conf_file)
+        write_config_file(params=params, output_conf_file=output_conf)
+        check_call(f"mpirun -n 2 pyrate conv2tif -f {output_conf}", shell=True, env=os.environ)
+        check_call(f"mpirun -n 2 pyrate prepifg -f {output_conf}", shell=True, env=os.environ)
+        check_call(f"mpirun -n 2 pyrate correct -f {output_conf}", shell=True, env=os.environ)
+        check_call(f"mpirun -n 2 pyrate timeseries -f {output_conf}", shell=True, env=os.environ)
+        check_call(f"mpirun -n 2 pyrate stack -f {output_conf}", shell=True, env=os.environ)
 
+        params = Configuration(output_conf).__dict__
+        cls.params = params
+
+    @classmethod
+    def teardown_class(cls):
+        shutil.rmtree(cls.params[C.OUT_DIR], ignore_errors=True)
+
+    def test_los_conversion_comparison(self):
+        """
+        compare outputs in each of the los projection types
+        compares sine and cosine components are larger than LOS component
+        assert metadata equal except
+        """
+        params = self.params
+        all_dirs = {}
+        for k in LOS_PROJECTION_OPTION.keys():
+            params[C.LOS_PROJECTION] = k
+            _merge_stack(params)
+            _merge_linrate(params)
+            _merge_timeseries(params, 'tscuml')
+            _merge_timeseries(params, 'tsincr')
+            k_dir = Path(params[C.OUT_DIR]).joinpath(ifc.LOS_PROJECTION_OPTION[k])
+            k_dir.mkdir(exist_ok=True)
+
+            for out_type in los_projection_out_types:
+                for tif in Path(params[C.OUT_DIR]).glob(out_type + '*.tif'):
+                    shutil.move(tif, k_dir.joinpath(tif.name))
+            all_dirs[k] = k_dir
+
+        los_proj_dir = all_dirs[ifc.LINE_OF_SIGHT]
+        pseudo_ver = all_dirs[ifc.PSEUDO_VERTICAL]
+        pseudo_hor = all_dirs[ifc.PSEUDO_HORIZONTAL]
+        assert len(list(los_proj_dir.glob('*.tif'))) == 26  # 12 tsincr, 12 tscuml + 1 stack rate + 1 linear rate
+        for tif in los_proj_dir.glob('*.tif'):
+            ds = DEM(tif)
+            ds_ver = DEM(pseudo_ver.joinpath(tif.name))
+            ds_hor = DEM(pseudo_hor.joinpath(tif.name))
+            ds.open()
+            ds_ver.open()
+            ds_hor.open()
+            non_nans_indices = ~np.isnan(ds.data)
+            # assert division by sine and cosine always yields larger components in vertical and horizontal directions
+            assert np.all(np.abs(ds.data[non_nans_indices]) < np.abs(ds_ver.data[non_nans_indices]))
+            assert np.all(np.abs(ds.data[non_nans_indices]) < np.abs(ds_hor.data[non_nans_indices]))
+            ds_md = ds.dataset.GetMetadata()
+            assert ds_md.pop(C.LOS_PROJECTION.upper()) == ifc.LOS_PROJECTION_OPTION[ifc.LINE_OF_SIGHT]
+            ds_ver_md = ds_ver.dataset.GetMetadata()
+            assert ds_ver_md.pop(C.LOS_PROJECTION.upper()) == ifc.LOS_PROJECTION_OPTION[ifc.PSEUDO_VERTICAL]
+            assert ds_md == ds_ver_md
+            ds_hor_md = ds_hor.dataset.GetMetadata()
+            assert ds_hor_md.pop(C.LOS_PROJECTION.upper()) == ifc.LOS_PROJECTION_OPTION[ifc.PSEUDO_HORIZONTAL]
+            assert ds_md == ds_hor_md
+
+    def test_file_creation(self, los_projection):
+        params = self.params
+        params[C.LOS_PROJECTION] = los_projection
+        try:
+            _merge_stack(params)
+            _merge_linrate(params)
+            _merge_timeseries(params, 'tscuml')
+            _merge_timeseries(params, 'tsincr')
+        except RuntimeError:
+            return
+        # check if color map is created
+        for ot in ['stack_rate', 'stack_error', 'linear_rate', 'linear_error', 'linear_rsquared']:
+            create_png_and_kml_from_tif(params[C.OUT_DIR], output_type=ot)
+            output_color_map_path = os.path.join(params[C.OUT_DIR], f"colourmap_{ot}.txt")
+            assert Path(output_color_map_path).exists(), "Output color map file not found at: " + output_color_map_path
+
+        # check if merged files are created
+        for _type, ot in itertools.product(['stack_rate', 'stack_error', 'linear_rate',
+                                            'linear_error', 'linear_rsquared'], ['.tif', '.png', '.kml']):
+            output_image_path = os.path.join(params[C.OUT_DIR], _type + ot)
+            print(f"checking {output_image_path}")
+            assert Path(output_image_path).exists(), f"Output {ot} file not found at {output_image_path}"
+
+        # check los_projection metadata
         for out_type in los_projection_out_types:
             for tif in Path(params[C.OUT_DIR]).glob(out_type + '*.tif'):
-                shutil.move(tif, k_dir.joinpath(tif.name))
-        all_dirs[k] = k_dir
+                self.__check_md(los_projection, tif.as_posix())
 
-    los_proj_dir = all_dirs[ifc.LINE_OF_SIGHT]
-    pseudo_ver = all_dirs[ifc.PSEUDO_VERTICAL]
-    pseudo_hor = all_dirs[ifc.PSEUDO_HORIZONTAL]
-    assert len(list(los_proj_dir.glob('*.tif'))) == 26  # 12 tsincr, 12 tscuml + 1 stack rate + 1 linear rate
-    for tif in los_proj_dir.glob('*.tif'):
-        ds = DEM(tif)
-        ds_ver = DEM(pseudo_ver.joinpath(tif.name))
-        ds_hor = DEM(pseudo_hor.joinpath(tif.name))
-        ds.open()
-        ds_ver.open()
-        ds_hor.open()
-        non_nans_indices = ~np.isnan(ds.data)
-        # assert division by sine and cosine always yields larger components in vertical and horizontal directions
-        assert np.all(np.abs(ds.data[non_nans_indices]) < np.abs(ds_ver.data[non_nans_indices]))
-        assert np.all(np.abs(ds.data[non_nans_indices]) < np.abs(ds_hor.data[non_nans_indices]))
-        ds_md = ds.dataset.GetMetadata()
-        assert ds_md.pop(C.LOS_PROJECTION.upper()) == ifc.LOS_PROJECTION_OPTION[ifc.LINE_OF_SIGHT]
-        ds_ver_md = ds_ver.dataset.GetMetadata()
-        assert ds_ver_md.pop(C.LOS_PROJECTION.upper()) == ifc.LOS_PROJECTION_OPTION[ifc.PSEUDO_VERTICAL]
-        assert ds_md == ds_ver_md
-        ds_hor_md = ds_hor.dataset.GetMetadata()
-        assert ds_hor_md.pop(C.LOS_PROJECTION.upper()) == ifc.LOS_PROJECTION_OPTION[ifc.PSEUDO_HORIZONTAL]
-        assert ds_md == ds_hor_md
-
-
-@pytest.mark.mpi
-@pytest.mark.slow
-@pytest.mark.skipif(not PY37GDAL302, reason="Only run in one CI env")
-def test_file_creation(create_pre_merge_output, los_projection):
-    params = create_pre_merge_output
-    params[C.LOS_PROJECTION] = los_projection
-    try:
-        _merge_stack(params)
-        _merge_linrate(params)
-        _merge_timeseries(params, 'tscuml')
-        _merge_timeseries(params, 'tsincr')
-    except RuntimeError:
-        return
-    # check if color map is created
-    for ot in ['stack_rate', 'stack_error', 'linear_rate', 'linear_error', 'linear_rsquared']:
-        create_png_and_kml_from_tif(params[C.OUT_DIR], output_type=ot)
-        output_color_map_path = os.path.join(params[C.OUT_DIR], f"colourmap_{ot}.txt")
-        assert Path(output_color_map_path).exists(), "Output color map file not found at: " + output_color_map_path
-
-    # check if merged files are created
-    for _type, ot in itertools.product(['stack_rate', 'stack_error', 'linear_rate',
-                                        'linear_error', 'linear_rsquared'], ['.tif', '.png', '.kml']):
-        output_image_path = os.path.join(params[C.OUT_DIR], _type + ot)
-        print(f"checking {output_image_path}")
-        assert Path(output_image_path).exists(), f"Output {ot} file not found at {output_image_path}"
-
-    # check los_projection metadata
-    for out_type in los_projection_out_types:
-        for tif in Path(params[C.OUT_DIR]).glob(out_type + '*.tif'):
-            __check_md(los_projection, tif.as_posix())
-    shutil.rmtree(params[C.OUT_DIR], ignore_errors=True)
-
-
-def __check_md(los_projection, output_image_path):
-    ifg = DEM(output_image_path)
-    ifg.open()
-    assert ifg.dataset.GetMetadataItem(C.LOS_PROJECTION.upper()) == LOS_PROJECTION_OPTION[los_projection]
+    @staticmethod
+    def __check_md(los_projection, output_image_path):
+        ifg = DEM(output_image_path)
+        ifg.open()
+        assert ifg.dataset.GetMetadataItem(C.LOS_PROJECTION.upper()) == LOS_PROJECTION_OPTION[los_projection]
