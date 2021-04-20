ad28ea96693be375ff5515fcaf98235c51c6b0a0
Sudipta Basak <basaks@gmail.com>
2021-04-20 21:56:06 +1000

    ifgs plot script available with pyrate installation [ci skip]

--- scripts/plot_ifgs.py
+++ scripts/plot_ifgs.py
@@ -20,18 +20,14 @@ This Python module defines executable run configuration for the PyRate software
 import os
 import argparse
 from argparse import RawTextHelpFormatter
-import time
 from pathlib import Path
-
+import numpy as np
 import pyrate.constants as C
 from pyrate.constants import CLI_DESCRIPTION
-from pyrate import conv2tif, prepifg, correct, merge
 from pyrate.core.logger import pyratelogger as log, configure_stage_log
 from pyrate.core import mpiops
+from pyrate.core.shared import Ifg, InputTypes
 from pyrate.configuration import Configuration
-from pyrate.core.shared import mpi_vs_multiprocess_logging
-from pyrate.core.stack import stack_calc_wrapper
-from pyrate.core.timeseries import timeseries_calc_wrapper
 
 
 def _params_from_conf(config_file):
@@ -42,8 +38,6 @@ def _params_from_conf(config_file):
 
 
 def main():
-
-    start_time = time.time()
 
     parser = argparse.ArgumentParser(prog='pyrate', description=CLI_DESCRIPTION, add_help=True,
                                      formatter_class=RawTextHelpFormatter)
@@ -67,7 +61,50 @@ def main():
     log.debug("Arguments supplied at command line: ")
     log.debug(args)
 
-    log.info("--- Runtime = %s seconds ---" % (time.time() - start_time))
+    try:
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+        cmap = mpl.cm.Spectral
+    except ImportError as e:
+        log.warn(ImportError(e))
+        log.warn("Required plotting packages are not found in environment. "
+                 "Please install matplotlib in your environment to continue plotting!!!")
+        return
+
+    ifgs = params[C.INTERFEROGRAM_FILES]
+    num_ifgs = len(ifgs)
+
+    plt_rows = np.int(np.sqrt(num_ifgs))
+    plt_cols = num_ifgs//plt_rows
+    if num_ifgs % plt_rows:
+        plt_cols += 1
+
+    fig = plt.figure(figsize=(12*plt_rows, 8*plt_cols))
+
+    tot_plots = 1
+    for p_r in range(plt_rows):
+        for p_c in range(plt_cols):
+            ax = fig.add_subplot(plt_rows, plt_cols, tot_plots)
+            ifg_num = plt_cols * p_r + p_c
+            m_path = ifgs[ifg_num]
+            if m_path.input_type == InputTypes.IFG:
+                ifg = Ifg(ifgs[ifg_num].converted_path)
+            else:
+                raise AttributeError("Can only plot tifs")
+            ifg.open()
+            im = ax.imshow(ifg.phase_data, cmap=cmap)
+            text = ax.set_title(Path(ifg.data_path).stem)
+            text.set_fontsize(min(20, int(num_ifgs/3)))
+
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            plt.colorbar(im, cax=cax)
+            if tot_plots == num_ifgs:
+                break
+            tot_plots += 1
+
+    plt.show()
 
 
 if __name__ == "__main__":
