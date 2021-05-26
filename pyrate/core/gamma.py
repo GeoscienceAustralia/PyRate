84c0685de53fa0cdb77183014b6bd44a79cf92ca
Alistair Deane <Alistair.Deane@ga.gov.au>
2021-05-26 17:04:21 +1000

    add exception for to many input par files (> 2)

--- pyrate/core/gamma.py
+++ pyrate/core/gamma.py
@@ -463,6 +463,8 @@ def manage_headers(dem_header_file, header_paths, baseline_paths=None):
         else:
 #            baseline_header = {}
              combined_header = combine_headers(hdrs[0], hdrs[1], dem_header)
+    elif len(header_paths) > 2:
+        raise GammaException('Too many parameter files given for one interferogram; there should be only two')
     else:
         # probably have DEM or incidence file
         combined_header = dem_header
