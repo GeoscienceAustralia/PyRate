Better handling of millimetres/radians when reading/writing during correct step

pyrate/core/aps.py
@@ -34,7 +34,7 @@ from pyrate.core.logger import pyratelogger as log
 from pyrate.core import shared, ifgconstants as ifc, mpiops
 from pyrate.core.covariance import cvd_from_phase, RDist
 from pyrate.core.algorithm import get_epochs
-from pyrate.core.shared import Ifg, Tile, EpochList
+from pyrate.core.shared import Ifg, Tile, EpochList, nan_and_mm_convert
 from pyrate.core.timeseries import time_series
 from pyrate.merge import assemble_tiles
 from pyrate.configuration import MultiplePaths, Configuration
@@ -68,7 +68,7 @@ def spatio_temporal_filter(params: dict) -> None:
     aps_paths = [MultiplePaths.aps_error_path(i, params) for i in ifg_paths]
     if all(a.exists() for a in aps_paths):
         log.warning('Reusing APS errors from previous run')
-        _apply_aps_correction(ifg_paths, aps_paths)
+        _apply_aps_correction(ifg_paths, aps_paths, params)
         return
 
     # obtain the incremental time series using SVD
@@ -92,7 +92,7 @@ def spatio_temporal_filter(params: dict) -> None:
     _make_aps_corrections(ts_aps, ifgs, params)
 
     # apply correction to ifgs and save ifgs to disc.
-    _apply_aps_correction(ifg_paths, aps_paths)
+    _apply_aps_correction(ifg_paths, aps_paths, params)
 
     # update/save the phase_data in the tiled numpy files
     shared.save_numpy_phase(ifg_paths, params)
@@ -172,7 +172,7 @@ def _make_aps_corrections(ts_aps: np.ndarray, ifgs: List[Ifg], params: dict) -> 
     mpiops.comm.barrier()
 
 
-def _apply_aps_correction(ifg_paths: List[str], aps_paths: List[str]) -> None:
+def _apply_aps_correction(ifg_paths: List[str], aps_paths: List[str], params: dict) -> None:
     """
     Function to read and apply (subtract) APS corrections from interferogram data.
     """
@@ -182,6 +182,8 @@ def _apply_aps_correction(ifg_paths: List[str], aps_paths: List[str]) -> None:
         # open the Ifg object
         ifg = Ifg(ifg_path)
         ifg.open(readonly=False)
+        # convert NaNs and convert to mm
+        nan_and_mm_convert(ifg, params)
         # subtract the correction from the ifg phase data
         ifg.phase_data[~np.isnan(ifg.phase_data)] -= aps_corr[~np.isnan(ifg.phase_data)]
         # set meta-data tags after aps error correction

pyrate/core/phase_closure/closure_check.py
@@ -25,7 +25,7 @@ from pyrate.core.phase_closure.mst_closure import sort_loops_based_on_weights_an
 from pyrate.configuration import Configuration, MultiplePaths
 from pyrate.core.phase_closure.sum_closure import sum_phase_closures
 from pyrate.core.phase_closure.plot_closure import plot_closure
-from pyrate.core.shared import Ifg
+from pyrate.core.shared import Ifg, nan_and_mm_convert
 from pyrate.core.logger import pyratelogger as log
 
 
@@ -34,23 +34,22 @@ def mask_pixels_with_unwrapping_errors(ifgs_breach_count: NDArray[(Any, Any, Any
                                        params: dict) -> None:
     """
     Find pixels in the phase data that breach closure_thr, and mask
-    (assign nans) to those pixels in those ifgs.
+    (assign NaNs) to those pixels in those ifgs.
     :param ifgs_breach_count: unwrapping issues at pixels in all loops
     :param num_occurrences_each_ifg:  frequency of ifgs appearing in all loops
     :param params: params dict
     """
-    log.debug("Updating phase data of retained ifgs")
+    log.debug("Masking phase data of retained ifgs")
 
     for i, m_p in enumerate(params[C.INTERFEROGRAM_FILES]):
         pix_index = ifgs_breach_count[:, :, i] == num_occurrences_each_ifg[i]
         ifg = Ifg(m_p.tmp_sampled_path)
         ifg.open()
-        ifg.nodata_value = params[C.NO_DATA_VALUE]
-        ifg.convert_to_nans()
+        nan_and_mm_convert(ifg, params)
         ifg.phase_data[pix_index] = np.nan
         ifg.write_modified_phase()
 
-    log.info(f"Updated phase data of {i + 1} retained ifgs after phase closure")
+    log.info(f"Masked phase data of {i + 1} retained ifgs after phase closure")
     return None
 
 